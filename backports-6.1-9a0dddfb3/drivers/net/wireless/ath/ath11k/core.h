b5fffc6d65b0984a3d69dd1af0d74271dfe7351a
Miles Hu <milehu@qti.qualcomm.com>
2019-10-11 19:24:06 -0700

    ath11k: add HE stats in peer stats packet counters for MIMO and OFDMA
    Change-Id: I6d75bd604a4e66dd899860693ee5a043c9172aae
    Signed-off-by: Miles Hu <milehu@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/core.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/core.h
@@ -35,6 +35,7 @@
 #include "fw.h"
 #include "coredump.h"
 #include "vendor.h"
+#include "rx_desc.h"
 
 #ifdef CONFIG_QCOM_QMI_HELPERS
 extern wait_queue_head_t ath11k_ssr_dump_wq;
@@ -532,6 +533,8 @@ struct ath11k_htt_data_stats {
 	u64 bw[ATH11K_COUNTER_TYPE_MAX][ATH11K_BW_NUM];
 	u64 nss[ATH11K_COUNTER_TYPE_MAX][ATH11K_NSS_NUM];
 	u64 gi[ATH11K_COUNTER_TYPE_MAX][ATH11K_GI_NUM];
+	u64 transmit_type[ATH11K_COUNTER_TYPE_MAX][HAL_RX_RECEPTION_TYPE_MAX];
+	u64 ru_loc[ATH11K_COUNTER_TYPE_MAX][HAL_RX_RU_ALLOC_TYPE_MAX];
 };
 
 struct ath11k_htt_tx_stats {
@@ -539,6 +542,9 @@ struct ath11k_htt_tx_stats {
 	u64 tx_duration;
 	u64 ba_fails;
 	u64 ack_fails;
+	u16 ru_start;
+	u16 ru_tones;
+	u32 mu_group[MAX_MU_GROUP_ID];
 };
 
 struct ath11k_per_ppdu_tx_stats {
@@ -660,11 +666,16 @@ struct ath11k_per_peer_tx_stats {
 	u32 succ_bytes;
 	u32 retry_bytes;
 	u32 failed_bytes;
+	u32 duration;
 	u16 succ_pkts;
 	u16 retry_pkts;
 	u16 failed_pkts;
-	u32 duration;
+	u16 ru_start;
+	u16 ru_tones;
 	u8 ba_fails;
+	u8 ppdu_type;
+	u32 mu_grpid;
+	u32 mu_pos;
 	bool is_ampdu;
 };
 
