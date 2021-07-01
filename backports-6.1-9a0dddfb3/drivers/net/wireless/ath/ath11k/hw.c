42600d477bbe1dd1db9bcb6abf6d024fe9a8693e
Sathishkumar Muruganandam <murugana@qti.qualcomm.com>
2021-07-01 13:08:54 +0530

    ath11k: fix double free of peer rx_tid during reo cmd failure
    Peer rx_tid is locally copied thrice during peer_rx_tid_cleanup to send
    REO_CMD_UPDATE_RX_QUEUE followed by REO_CMD_FLUSH_CACHE to flush all
    aged REO descriptors from HW cache.
    
    When sending REO_CMD_FLUSH_CACHE fails, we do dma unmap of already
    mapped rx_tid->vaddr and free it. This is not checked during
    reo_cmd_list_cleanup() and dp_reo_cmd_free() before trying to free and
    unmap again.
    
    Fix this by setting rx_tid->vaddr NULL in rx tid delete and also wherever
    freeing it to check in reo_cmd_list_cleanup() and reo_cmd_free() before
    trying to free again.
    
    Prevent REO cmd failures causing double free by increasing REO cmd
    ring size and moving REO status ring mask to IRQ group 3 from group
    0 to separate from tx completion ring on IRQ group 0 which may delay
    reo status processing.
    
    Change-Id: Ia3029749e11ea6c82fabeae6413abb9d51cebe26
    Signed-off-by: Sathishkumar Muruganandam <murugana@qti.qualcomm.com>
    Signed-off-by: Kaviarasu Sanjeevi <kaviaras@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/hw.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/hw.c
@@ -1712,6 +1712,7 @@ const struct ath11k_hw_ring_mask ath11k_hw_ring_mask_qca6390 = {
 		ATH11K_RX_WBM_REL_RING_MASK_0,
 	},
 	.reo_status = {
+		0, 0, 0,
 		ATH11K_REO_STATUS_RING_MASK_0,
 	},
 	.rxdma2host = {
