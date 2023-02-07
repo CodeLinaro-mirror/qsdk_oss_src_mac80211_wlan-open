e54412ca0d749f44cbca6a28c7ffc308d8cc2e72
Thiraviyam Mariyappan <tmariyap@qti.qualcomm.com>
2023-02-07 09:52:10 +0530

    ath12k: Add support to set tx power for 6G via iw
    Add support to set the tx power for 6G radio with the wmi
    pdev param WMI_PDEV_PARAM_TXPOWER_LIMIT5G via iw command.
    iw dev wlanX set txpower fixed 1000
    TEST=With the above iw command and verified the tx power
    value in the iw dev for 6G radio is set by the configured
    value.
    
    Change-Id: I7d88af559d80b65de10b7bdb0902f6cef9d05549
    Signed-off-by: Thiraviyam Mariyappan <tmariyap@qti.qualcomm.com>
    Signed-off-by: Maharaja Kennadyrajan <quic_mkenna@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
@@ -833,6 +833,7 @@ struct ath12k {
 	u32 max_tx_power;
 	u32 txpower_limit_2g;
 	u32 txpower_limit_5g;
+	u32 txpower_limit_6g;
 	u32 txpower_scale;
 	u32 power_scale;
 	u32 chan_tx_pwr;
--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
@@ -1029,6 +1029,7 @@ static void ath12k_pdev_caps_update(struct ath12k *ar)
 
 	ar->txpower_limit_2g = ar->max_tx_power;
 	ar->txpower_limit_5g = ar->max_tx_power;
+	ar->txpower_limit_6g = ar->max_tx_power;
 	ar->txpower_scale = WMI_HOST_TP_SCALE_MAX;
 }
 
@@ -1079,6 +1080,16 @@ static int ath12k_mac_txpower_recalc(struct ath12k *ar)
 		if (ret)
 			goto fail;
 		ar->txpower_limit_5g = txpower;
+	}
+
+	if ((ar->ah->hw->wiphy->bands[NL80211_BAND_6GHZ]) &&
+		ar->txpower_limit_6g != txpower) {
+		param = WMI_PDEV_PARAM_TXPOWER_LIMIT5G;
+		ret = ath12k_wmi_pdev_set_param(ar, param,
+						txpower, ar->pdev->pdev_id);
+		if (ret)
+			goto fail;
+		ar->txpower_limit_6g = txpower;
 	}
 
 	return 0;
@@ -5206,19 +5217,8 @@ skip_pending_cs_up:
 	if (changed & BSS_CHANGED_TXPOWER) {
 		ath12k_dbg(ar->ab, ATH12K_DBG_MAC, "mac vdev_id %i txpower %d\n",
 			   arvif->vdev_id, info->txpower);
-
-		if (ar->supports_6ghz && info->chanreq.oper.chan &&
-                    info->chanreq.oper.chan->band == NL80211_BAND_6GHZ &&
-                    (ahvif->vdev_type == WMI_VDEV_TYPE_STA ||
-                     ahvif->vdev_type == WMI_VDEV_TYPE_AP) &&
-                    test_bit(WMI_TLV_SERVICE_EXT_TPC_REG_SUPPORT,
-                             ar->ab->wmi_ab.svc_map)) {
-                        ath12k_dbg(ar->ab, ATH12K_DBG_MAC,
-                                   "discard tx power, change to set TPC power\n");
-                } else {
-                        arvif->txpower = info->txpower;
-                        ath12k_mac_txpower_recalc(ar);
-                }
+		arvif->txpower = info->txpower;
+		ath12k_mac_txpower_recalc(ar);
 	}
 
 	if (changed & BSS_CHANGED_MCAST_RATE &&
