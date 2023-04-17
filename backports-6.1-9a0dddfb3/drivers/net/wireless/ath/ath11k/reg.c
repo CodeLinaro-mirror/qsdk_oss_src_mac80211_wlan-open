227a8e71e400ee15d696073e8a11dc7e5229eb2a
Karthik M <quic_karm@quicinc.com>
2023-04-17 17:22:39 +0530

    ath11k: Fix the wrong regdomain update for KR country
    KR country should follow JP regulatory domain instead it
    was assigned ETSI domain wrongly.
    
    This issue got exposed after the change
    5dbddc3d4ac ("cfg80211: precac check for self-managed wiphy")
    got merged.
    
    Change-Id: I9015e1b22e0529d818626e95620958340c439380
    Signed-off-by: Karthik M <quic_karm@quicinc.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/reg.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/reg.c
@@ -232,10 +232,10 @@ ath11k_map_fw_dfs_region(enum ath11k_dfs_region dfs_region)
 	case ATH11K_DFS_REG_CN:
 		return NL80211_DFS_FCC;
 	case ATH11K_DFS_REG_ETSI:
-	case ATH11K_DFS_REG_KR:
 		return NL80211_DFS_ETSI;
 	case ATH11K_DFS_REG_MKK:
 	case ATH11K_DFS_REG_MKK_N:
+	case ATH11K_DFS_REG_KR:
 		return NL80211_DFS_JP;
 	default:
 		return NL80211_DFS_UNSET;
