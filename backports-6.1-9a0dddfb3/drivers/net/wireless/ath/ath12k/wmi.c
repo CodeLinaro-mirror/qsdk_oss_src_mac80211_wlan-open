dd7a4961dfcaf880a55591cb2901c9afa06f166d
Suraj P Kizhakkethil <quic_surapk@quicinc.com>
2025-02-04 11:04:50 +0530

    wifi: ath12k: Pass correct values of center freq1 and center freq2 for 160 MHz
    Currently, for 160 MHz bandwidth, center frequency1 and
    center frequency2 are not passed correctly to the firmware.
    Set center frequency1 as the center frequency
    of the primary 80 MHz channel segment and center frequency2 as
    the center frequency of the 160 MHz channel and pass the values
    to the firmware.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    
    Change-Id: I72576888c4430eeb32baadf874ce298e2c590e02
    Signed-off-by: Suraj P Kizhakkethil <quic_surapk@quicinc.com>
    Reviewed-by: Aditya Kumar Singh <aditya.kumar.singh@oss.qualcomm.com>
    Signed-off-by: Raj Kumar Bhagat <quic_rajkbhag@quicinc.com>
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.c
@@ -1054,7 +1054,7 @@ static void ath12k_wmi_put_wmi_channel(struct ath12k_wmi_channel_params *chan,
 
 	chan->mhz = cpu_to_le32(arg->freq);
 	chan->band_center_freq1 = cpu_to_le32(arg->band_center_freq1);
-	if (arg->mode == MODE_11AX_HE160) {
+	if (arg->mode == MODE_11BE_EHT160) {
 		if (arg->freq > center_freq1)
 			chan->band_center_freq1 =
 					cpu_to_le32(center_freq1 + 40);
@@ -1063,6 +1063,8 @@ static void ath12k_wmi_put_wmi_channel(struct ath12k_wmi_channel_params *chan,
 					cpu_to_le32(center_freq1 - 40);
 
 		chan->band_center_freq2 = cpu_to_le32(arg->band_center_freq1);
+	} else if (arg->mode == MODE_11BE_EHT80_80) {
+		chan->band_center_freq2 = cpu_to_le32(arg->band_center_freq2);
 	} else {
 		chan->band_center_freq2 = 0;
 	}
