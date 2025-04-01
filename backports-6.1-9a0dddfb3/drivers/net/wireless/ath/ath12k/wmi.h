904bd22400a2a62c488a928b1c79ab349e853f88
Avula Sri Charan <quic_asrichar@quicinc.com>
2025-04-01 23:44:48 +0530

    wifi: ath12k: fix handling of 6 GHz rules
    In the US country code, to avoid including 6 GHz rules in the 5 GHz rules
    list, the number of 5 GHz rules is set to a default constant value of 4
    (REG_US_5G_NUM_REG_RULES). However, if there are more than 4 valid 5 GHz
    rules, the current logic will bypass the legitimate 6 GHz rules.
    
    For example, if there are 5 valid 5 GHz rules and 1 valid 6 GHz rule, the
    current logic will only consider 4 of the 5 GHz rules, treating the last
    valid rule as a 6 GHz rule. Consequently, the actual 6 GHz rule is never
    processed, leading to the eventual disabling of 6 GHz channels.
    
    To fix this issue, instead of hardcoding the value to 4, use a helper
    function to determine the number of 6 GHz rules present in the 5 GHz rules
    list and ignore only those rules.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    
    Link: https://patch.msgid.link/20250123-fix_6ghz_rules_handling-v1-1-d734bfa58ff4@oss.qualcomm.com
    Change-Id: If9ee3ca97656e31b6c584795d104f3017479f4a1
    Signed-off-by: Aditya Kumar Singh <aditya.kumar.singh@oss.qualcomm.com>
    Signed-off-by: Avula Sri Charan <quic_asrichar@quicinc.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
@@ -4193,7 +4193,6 @@ struct ath12k_wmi_eht_rate_set_params {
 #define MAX_REG_RULES 10
 #define REG_ALPHA2_LEN 2
 #define MAX_6GHZ_REG_RULES 5
-#define REG_US_5G_NUM_REG_RULES 4
 
 struct wmi_set_current_country_arg {
 	u8 alpha2[REG_ALPHA2_LEN];
