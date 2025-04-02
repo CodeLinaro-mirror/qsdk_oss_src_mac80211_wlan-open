cae636f2a754e7bee2498eefb62536bf1bf7dd6c
Balamurugan Ramar <quic_bramar@quicinc.com>
2025-04-02 18:13:08 +0530

    mac80211: Add compatability fix for select_queue
    Add compatibility fix for ieee80211_monitor_select_queue
    for different kernel versions by adjusting
    the function parameters accordingly
    
    Change-Id: Ic21e0e691dd8bd5c3a4979039ad37b2fd2eedc84
    Signed-off-by: Balamurugan Ramar <quic_bramar@quicinc.com>
    Signed-off-by: Kaviarasu Sanjeevi <quic_kaviaras@quicinc.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/net/mac80211/iface.c
+++ backports-6.1-9a0dddfb3/net/mac80211/iface.c
@@ -845,9 +845,21 @@ static const struct net_device_ops ieee80211_dataif_ops = {
 	.ndo_setup_tc		= ieee80211_netdev_setup_tc,
 };
 
+#if LINUX_VERSION_IS_GEQ(5,2,0)
 static u16 ieee80211_monitor_select_queue(struct net_device *dev,
 					  struct sk_buff *skb,
 					  struct net_device *sb_dev)
+#elif LINUX_VERSION_IS_GEQ(4,19,0)
+static u16 ieee80211_monitor_select_queue(struct net_device *dev,
+					  struct sk_buff *skb,
+					  struct net_device *sb_dev,
+					  select_queue_fallback_t fallback)
+#else
+static u16 ieee80211_monitor_select_queue(struct net_device *dev,
+					  struct sk_buff *skb,
+					  void *accel_priv,
+					  select_queue_fallback_t fallback)
+#endif
 {
 	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
 	struct ieee80211_local *local = sdata->local;
