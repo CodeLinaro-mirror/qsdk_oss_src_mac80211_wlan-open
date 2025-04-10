a460990395505d6625e2e58132ebe225eabf9b0a
Balamurugan Ramar <quic_bramar@quicinc.com>
2025-04-10 23:26:26 +0530

    mac80211: wifi set all links active for station by default
    Change-Id: I27820265033d7b1fbeb7256e58dee5693be09eeb
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Amutha Ravikumar <quic_aravikum@quicinc.com>
    Signed-off-by: Balamurugan Ramar <quic_bramar@quicinc.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/net/mac80211/link.c
+++ backports-6.1-9a0dddfb3/net/mac80211/link.c
@@ -282,17 +282,9 @@ static void ieee80211_set_vif_links_bitmaps(struct ieee80211_sub_if_data *sdata,
 	switch (sdata->vif.type) {
 	case NL80211_IFTYPE_AP:
 	case NL80211_IFTYPE_AP_VLAN:
-		/* in an AP all links are always active */
-		sdata->vif.active_links = valid_links;
-
-		/* AP links are not expected to be disabled */
-		WARN_ON(dormant_links);
-		break;
 	case NL80211_IFTYPE_STATION:
-		if (sdata->vif.active_links)
-			break;
+		/* in an AP and STA all links are always active */
 		sdata->vif.active_links = valid_links & ~dormant_links;
-		WARN_ON(hweight16(sdata->vif.active_links) > 1);
 		break;
 	default:
 		WARN_ON(1);
