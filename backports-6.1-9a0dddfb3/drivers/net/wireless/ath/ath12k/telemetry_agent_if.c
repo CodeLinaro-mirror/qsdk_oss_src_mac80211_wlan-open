2a1a7eabe33471601cac95abb4d1d68ee697deca
Balamurugan S <bselvara@qti.qualcomm.com>
2025-05-21 09:52:38 +0530

    qsdk: CP: wifi: ath12k: Add stub functions for ecm module compilation
    Add ath_get_metadata_info() and ath_sawf_uplink() stub functions
    for ecm-wifi-plugin module compilation. This is temporary WAR
    and upon sawf functionality porting this patch will be removed.
    
    Patch-dependency: None
    
    Change-Id: Ibd994cbb1c86d0cadf9f99aeb1c9d4693f9bccf3
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/telemetry_agent_if.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/telemetry_agent_if.c
@@ -4,6 +4,8 @@
  */
 
 #include <linux/export.h>
+#include <ath/ath_dp_accel_cfg.h>
+#include <ath/ath_sawf.h>
 
 struct telemetry_agent_ops *g_agent_ops;
 
@@ -19,3 +21,14 @@ int unregister_telemetry_agent_ops(struct telemetry_agent_ops *agent_ops){
 }
 EXPORT_SYMBOL(unregister_telemetry_agent_ops);
 
+u32 ath_get_metadata_info(struct ath_dp_metadata_param *dp_metadata_param)
+{
+	        return false;
+}
+EXPORT_SYMBOL(ath_get_metadata_info);
+
+void ath_sawf_uplink(struct ath_ul_params *ecm_ath_ul_params)
+{
+	return;
+}
+EXPORT_SYMBOL(ath_sawf_uplink);
