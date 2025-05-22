b45799d2cef08d70fc51c8543418c96b9cc852f2
Balamurugan Mahalingam <bmahalin@qti.qualcomm.com>
2025-05-21 21:20:37 -0700

    QSDK:DP:wifi Add support for handling vendor command to config ppe vp type
    The vendor command interface allows the userspace application to
    configure and query the PPE VP type for a netdevice.
    The PPE VP type decides the datapath route the netdevice will utilize.
    
    Patch-dependency:
            325-QSDK-DP-wifi-Add-support-for-PPE-DS-Direct-switch-fe.patch
    Patch-work: None
    
    Change-Id: Icf4b17e8b2c24abc47147a3574f494d6eeffe746
    Signed-off-by: Balamurugan Mahalingam <bmahalin@qti.qualcomm.com>
    Signed-off-by: Bhagavathi Perumal S <bperumal@qti.qualcomm.com>
    Signed-off-by: Neha Bisht <nbisht@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

---
+++ drivers/net/wireless/ath/ath12k/vendor.h
@@ -0,0 +1,75 @@
+/* SPDX-License-Identifier: BSD-3-Clause-Clear */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+#ifndef ATH12K_VENDOR_H
+#define ATH12K_VENDOR_H
+
+#define QCA_NL80211_VENDOR_ID 0x001374
+
+extern unsigned int ath12k_ppe_ds_enabled;
+
+struct ath12k_wifi_generic_params {
+	u32 command;
+	u32 value;
+	void *data;
+	u32 data_len;
+	u32 length;
+	u32 flags;
+	u32 ifindex;
+	u8 link_id;
+};
+
+enum qca_nl80211_vendor_subcmds {
+	/* Wi-Fi configuration subcommand */
+	QCA_NL80211_VENDOR_SUBCMD_SET_WIFI_CONFIGURATION = 74,
+	QCA_NL80211_VENDOR_SUBCMD_GET_WIFI_CONFIGURATION = 75,
+	QCA_NL80211_VENDOR_SUBCMD_WIFI_PARAMS = 200,
+};
+
+enum qca_wlan_vendor_attr_config {
+	QCA_WLAN_VENDOR_ATTR_CONFIG_INVALID = 0,
+	/* Unsigned 32-bit attribute for generic commands */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_COMMAND = 17,
+	/* Unsigned 32-bit value attribute for generic commands */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_VALUE = 18,
+	/* Unsigned 32-bit data attribute for generic command response */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_DATA = 19,
+	/* Unsigned 32-bit length attribute for
+	 * QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_DATA
+	 */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_LENGTH = 20,
+	/* Unsigned 32-bit flags attribute for
+	 * QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_DATA
+	 */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_FLAGS = 21,
+	/* Unsigned 32-bit, specifies the interface index (netdev) for which the
+	 * corresponding configurations are applied. If the interface index is
+	 * not specified, the configurations are attributed to the respective
+	 * wiphy.
+	 */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_IFINDEX = 24,
+	/* 8-bit unsigned value. Used to specify the MLO link ID of a link
+	 * that is being configured. This attribute must be included in each
+	 * record nested inside %QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINKS, and
+	 * may be included without nesting to indicate the link that is the
+	 * target of other configuration attributes.
+	 */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINK_ID = 99,
+	/* 8-bit unsigned value to configure the interface offload type
+	 *
+	 * This attribute is used to configure the interface offload capability.
+	 * User can configure software based acceleration, hardware based
+	 * acceleration, or a combination of both using this option. More
+	 * details on each option is described under the enum definition below.
+	 * Uses enum qca_wlan_intf_offload_type for values.
+	 */
+	QCA_WLAN_VENDOR_ATTR_IF_OFFLOAD_TYPE = 120,
+	/* Keep last */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_AFTER_LAST,
+	QCA_WLAN_VENDOR_ATTR_CONFIG_MAX =
+		QCA_WLAN_VENDOR_ATTR_CONFIG_AFTER_LAST - 1
+};
+
+int ath12k_vendor_register(struct ath12k_hw *ah);
+#endif
