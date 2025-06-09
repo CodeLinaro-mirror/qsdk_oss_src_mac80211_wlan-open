c5f3d9b73cd002782b1f6ed59cf19432c5f7dca0
Alok Singh <aloksing@qti.qualcomm.com>
2025-06-09 13:00:09 +0530

    UPSTREAM: DP: wifi: ath12k: add ring config for monitor mode on WCN7850
    For WCN7850, it doesn't have an RX MON component(RX MON dest ring and
    RX MON buffer ring). So it can only reuse the mon status ring, RX
    refill ring, and error dst ring for monitor mode.
    
    Every time the hardware captures a packet, it will store status
    information into the status ring and store the MSDU payload buffer
    address into the error dst ring. When host reaps the MSDU payload and
    consumes a buffer, it will replenish the RX refill ring to indicate a
    new buffer.
    
    The configuration of the error dst/RX ring already exists. But there is
    no configuration for mon status ring. So add ring configuration for it.
    
    Patch-dependency:
    	0401-0003-UPSTREAM-DP-wifi-ath12k-add-srng-config-template-for.patch
    
    Change-Id: I2dc0173a939459d8055d78db40e16a2b72d96da5
    Signed-off-by: Alok Singh <aloksing@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp.h
@@ -272,6 +272,14 @@ struct ath12k_pdev_dp {
 #define DP_PPE_WBM2SW_RING_SIZE 8192
 #define HAL_REO2PPE_DST_IND 6
 
+#define RX_MON_STATUS_BASE_BUF_SIZE	2048
+#define RX_MON_STATUS_BUF_ALIGN		128
+#define RX_MON_STATUS_BUF_RESERVATION	128
+#define RX_MON_STATUS_BUF_SIZE		(RX_MON_STATUS_BASE_BUF_SIZE - \
+				 (RX_MON_STATUS_BUF_RESERVATION + \
+				  RX_MON_STATUS_BUF_ALIGN + \
+				  SKB_DATA_ALIGN(sizeof(struct skb_shared_info))))
+
 #define DP_RXDMA_BUF_COOKIE_BUF_ID	GENMASK(17, 0)
 #define DP_RXDMA_BUF_COOKIE_PDEV_ID	GENMASK(19, 18)
 
@@ -659,6 +667,7 @@ struct ath12k_dp {
 	struct dp_srng rxdma_err_dst_ring[MAX_RXDMA_PER_PDEV];
 	struct dp_rxdma_mon_ring rxdma_mon_buf_ring;
 	struct dp_rxdma_mon_ring tx_mon_buf_ring;
+	struct dp_rxdma_mon_ring rx_mon_status_refill_ring[MAX_RXDMA_PER_PDEV];
 	struct ath12k_reo_q_addr_lut reoq_lut;
 	struct ath12k_reo_q_addr_lut ml_reoq_lut;
 	const struct ath12k_hw_params *hw_params;
