230d992ddc5b3cc500b18eb80cb59a5c0ad37042
Rajat Soni <rajson@qti.qualcomm.com>
2025-07-07 15:25:48 +0530

    QSDK: CP: wifi: ath11k: Update ath11k_mac_op_set_antenna() arguments
    As part of dynamic chain mask change add one more argument in function
    ath11k_mac_op_set_antenna().
    
    Change-Id: Ib3c97666ba39faf2e922b5341ed396ef52c9d279
    Signed-off-by: Rajat Soni <rajson@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/mac.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/mac.c
@@ -8586,7 +8586,7 @@ static int ath11k_mac_op_get_antenna(struct ieee80211_hw *hw, u32 *tx_ant,
 }
 
 static int ath11k_mac_op_set_antenna(struct ieee80211_hw *hw, u32 tx_ant,
-				     u32 rx_ant, u8 radio_id)
+				     u32 rx_ant, u8 radio_id, bool is_dynamic)
 {
 	struct ath11k *ar = hw->priv;
 	int ret;
