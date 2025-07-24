bc01b5003712317cd9fa95ee23efeb40822d04b6
Guru Pratap Sharma <guruprat@qti.qualcomm.com>
2025-07-23 22:46:06 -0700

    UPSTREAM:CP: Fix compiler warnings
    This patch fix compile time warnings in wifi component
    
    Change-Id: I4b8df213aa9a411c6469d8152709dd0250eb708d
    Signed-off-by: Guru Pratap Sharma <guruprat@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/kconf/conf.c
+++ backports-6.1-9a0dddfb3/kconf/conf.c
@@ -86,7 +86,7 @@ static int conf_askvalue(struct symbol *sym, const char *def)
 	enum symbol_type type = sym_get_type(sym);
 
 	if (!sym_has_value(sym))
-		printf(_("(NEW) "));
+		printf("%s", _("(NEW) "));
 
 	line[0] = '\n';
 	line[1] = 0;
@@ -282,7 +282,7 @@ static int conf_choice(struct menu *menu)
 			if (child->sym->name)
 				printf(" (%s)", child->sym->name);
 			if (!sym_has_value(child->sym))
-				printf(_(" (NEW)"));
+				printf("%s", _(" (NEW)"));
 			printf("\n");
 		}
 		printf(_("%*schoice"), indent - 1, "");
@@ -437,7 +437,7 @@ static void check_conf(struct menu *menu)
 				}
 			} else {
 				if (!conf_cnt++)
-					printf(_("*\n* Restart config...\n*\n"));
+					printf("%s", _("*\n* Restart config...\n*\n"));
 				rootEntry = menu_get_parent_menu(menu);
 				conf(rootEntry);
 			}
@@ -613,7 +613,7 @@ int main(int ac, char **av)
 		if (conf_get_changed()) {
 			name = getenv("KCONFIG_NOSILENTUPDATE");
 			if (name && *name) {
-				fprintf(stderr,
+				fprintf(stderr, "%s",
 					_("\n*** The configuration requires explicit update.\n\n"));
 				return 1;
 			}
@@ -666,11 +666,13 @@ int main(int ac, char **av)
 		 * All other commands are only used to generate a config.
 		 */
 		if (conf_get_changed() && conf_write(NULL)) {
-			fprintf(stderr, _("\n*** Error during writing of the configuration.\n\n"));
+			fprintf(stderr, "%s",
+				_("\n*** Error during writing of the configuration\n\n"));
 			exit(1);
 		}
 		if (conf_write_autoconf()) {
-			fprintf(stderr, _("\n*** Error during update of the configuration.\n\n"));
+			fprintf(stderr, "%s",
+				_("\n*** Error during update of the configuration.\n\n"));
 			return 1;
 		}
 	} else if (input_mode == savedefconfig) {
@@ -681,7 +683,8 @@ int main(int ac, char **av)
 		}
 	} else if (input_mode != listnewconfig) {
 		if (conf_write(NULL)) {
-			fprintf(stderr, _("\n*** Error during writing of the configuration.\n\n"));
+			fprintf(stderr, "%s",
+				_("\n*** Error during writing of the configuration\n\n"));
 			exit(1);
 		}
 	}
