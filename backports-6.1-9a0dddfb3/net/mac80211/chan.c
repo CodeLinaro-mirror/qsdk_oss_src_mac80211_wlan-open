2463cc23e5f70aa3052512a2c1fadbc6feb04ed5
Rameshkumar Sundaram <ramess@qti.qualcomm.com>
2025-08-09 01:15:08 +0530

    UPSTREAM:PROTOCOL:CP: wifi: mac80211: assign vlan chanctx for non-ML vlan interfaces
    While assigning channel context for a master AP interface the same context
    should be copied to all the associated AP VLAN interfaces. However, for
    Non-ML AP VLAN interfaces this is not happening as vif->valid_links
    is checked against the AP VLAN link id on which the context is copied.
    
    For non-ML AP VLANs, vif->valid_links will not be set but
    vif->link_conf[0] will always hold the reference of vif->bss_conf and
    its default link id will be set to 0.
    
    As a result, TX packets from AP VLAN interfaces are getting dropped
    with below warning as they are scheduled for transmission without an
    active channel.
    
    WARNING: CPU: 0 PID: 8 at net/mac80211/tx.c:6542 ieee80211_tx_skb_tid+0xbc/0x17e [mac80211]
    Call Trace:
     ieee80211_tx_skb_tid+0x3d/0x17e [mac80211]
     ieee80211_tx_skb_tid+0xbc/0x17e [mac80211]
     __ieee80211_start_rx_ba_session+0x793/0xc34 [mac80211]
     sta_info_get_bss+0xa7/0xb4 [mac80211]
     ieee80211_process_addba_request+0x91/0x9e [mac80211]
     ieee80211_iface_work+0x194/0x5a6 [mac80211]
     process_one_work+0x211/0x477
     cfg80211_wiphy_work+0xb7/0xe4 [cfg80211]
    
    Check valid_links only for ML AP VLAN interfaces to allow copying master
    AP's context to both ML and non-ML AP VLAN interfaces properly.
    
    Patch-Dependency: 0141-wifi-mac80211-Changes-for-WDS-MLD.patch
    Patch-work: None
    
    Fixes: I2a6f3d80a30 ("cfg80211/mac80211: Validate link to use link conf")
    Change-Id: Id5e2a5c88e66fd2bd57a97289c8b4be96512651b
    Signed-off-by: Rameshkumar Sundaram <ramess@qti.qualcomm.com>
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/net/mac80211/chan.c
+++ backports-6.1-9a0dddfb3/net/mac80211/chan.c
@@ -1095,7 +1095,7 @@ __ieee80211_link_copy_chanctx_to_vlans(struct ieee80211_link_data *link,
 	list_for_each_entry(vlan, &sdata->u.ap.vlans, u.vlan.list) {
 		struct ieee80211_bss_conf *vlan_conf;
 
-		if (!(vlan->vif.valid_links & BIT(link_id)))
+		if (vlan->vif.valid_links && !(vlan->vif.valid_links & BIT(link_id)))
 			continue;
 
 		vlan_conf = wiphy_dereference(local->hw.wiphy,
@@ -1344,7 +1344,7 @@ ieee80211_link_update_chanreq(struct ieee80211_link_data *link,
 	list_for_each_entry(vlan, &sdata->u.ap.vlans, u.vlan.list) {
 		struct ieee80211_bss_conf *vlan_conf;
 
-		if (!(vlan->vif.valid_links & BIT(link_id)))
+		if (vlan->vif.valid_links && !(vlan->vif.valid_links & BIT(link_id)))
 			continue;
 
 		vlan_conf = wiphy_dereference(sdata->local->hw.wiphy,
