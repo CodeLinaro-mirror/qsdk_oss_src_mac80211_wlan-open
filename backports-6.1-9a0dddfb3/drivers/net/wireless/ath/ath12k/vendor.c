4f34e3f8aa587bb6d61dc33042b77667e02b9aa4
Balamurugan Mahalingam <bmahalin@qti.qualcomm.com>
2025-09-15 06:59:07 +0530

    QSDK:DP:wifi Add support for handling vendor command to config ppe vp type
    The vendor command interface allows the userspace application to
    configure and query the PPE VP type for a netdevice.
    The PPE VP type decides the datapath route the netdevice will utilize.
    
    Patch-dependency:
            325-QSDK-DP-wifi-Add-support-for-PPE-DS-Direct-switch-fe.patch
    Patch-work: None
    
    Change-Id: I7bf8d157f39cebab0d0091406117d1b49c20a712
    Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

---
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/vendor.c
@@ -0,0 +1,254 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. 
+ */
+#include <linux/mutex.h>
+#include <linux/pci.h>
+#include <linux/workqueue.h>
+#include <net/netlink.h>
+#include <net/mac80211.h>
+#include "core.h"
+#include "debug.h"
+#include "mac.h"
+#include "ppe.h"
+#include "vendor.h"
+
+static const struct nla_policy
+ath12k_wifi_config_policy[QCA_WLAN_VENDOR_ATTR_CONFIG_MAX + 1] = {
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_COMMAND] = {.type = NLA_U32 },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_VALUE] = {.type = NLA_U32 },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_DATA] = {.type = NLA_BINARY },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_LENGTH] = {.type = NLA_U32 },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_GENERIC_FLAGS] = {.type = NLA_U32 },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_IFINDEX] = {.type = NLA_U32 },
+	[QCA_WLAN_VENDOR_ATTR_CONFIG_MLO_LINK_ID] = {.type = NLA_U8 },
+	[QCA_WLAN_VENDOR_ATTR_IF_OFFLOAD_TYPE] = {.type = NLA_U8},
+};
+
+static int ath12k_vendor_wifi_config_handler(struct wiphy *wiphy,
+					     struct wireless_dev *wdev,
+					     const void *data, int data_len)
+{
+	struct nlattr *tb[QCA_WLAN_VENDOR_ATTR_CONFIG_MAX + 1];
+	struct ieee80211_vif *vif = NULL;
+	struct ath12k_vif *ahvif = NULL;
+	int ppe_vp_type = 0;
+	char *type = NULL;
+	int ret = 0;
+
+	ret = nla_parse(tb, QCA_WLAN_VENDOR_ATTR_CONFIG_MAX, data, data_len,
+			ath12k_wifi_config_policy, NULL);
+
+	if (ret) {
+		pr_err("Invalid attribute with vendor wifi config %d\n", ret);
+		return ret;
+	}
+
+	if (tb[QCA_WLAN_VENDOR_ATTR_IF_OFFLOAD_TYPE]) {
+		ppe_vp_type = nla_get_u8(tb[QCA_WLAN_VENDOR_ATTR_IF_OFFLOAD_TYPE]);
+
+	if (ppe_vp_type > PPE_VP_USER_TYPE_DS) {
+		ath12k_dbg(NULL, ATH12K_DBG_PPE, "ppe_vp_type value greater than 4 (%d)(%s)\n",
+			   ppe_vp_type, wdev->netdev->name);
+		return -EINVAL;
+	}
+
+	switch (ppe_vp_type) {
+	case PPE_VP_USER_TYPE_PASSIVE:
+		type = "passive";
+		break;
+	case PPE_VP_USER_TYPE_ACTIVE:
+		type = "active";
+		break;
+	case PPE_VP_USER_TYPE_DS:
+		type = "ds";
+		break;
+	default:
+		type = "passive";
+		ppe_vp_type = 1;
+		break;
+	}
+
+	if (!ath12k_ppe_ds_enabled) {
+		type = "passive";
+		ppe_vp_type = 1;
+		pr_err("Overriding offload type to passive as DS isn't enabled\n");
+	}
+
+	if (wdev->ppe_vp_type != ppe_vp_type)
+		wdev->ppe_vp_type = ppe_vp_type;
+	else
+		return ret;
+
+	vif = wdev_to_ieee80211_vif(wdev);
+	if (!vif) {
+		ath12k_dbg(NULL, ATH12K_DBG_PPE, "vif is NULL\n");
+		return ret;
+	}
+	if (vif->type == NL80211_IFTYPE_AP_VLAN) {
+		ath12k_dbg(NULL, ATH12K_DBG_PPE, "vif is AP_VLAN\n");
+		return ret;
+	}
+	ahvif = ath12k_vif_to_ahvif(vif);
+	if (!ahvif) {
+		ath12k_dbg(NULL, ATH12K_DBG_PPE, "ahvif is NULL\n");
+		return ret;
+	}
+	if (ppe_vp_type != ATH12K_INVALID_PPE_VP_TYPE &&
+	    ahvif->dp_vif.ppe_vp_num != ATH12K_INVALID_PPE_VP_NUM) {
+		ret = ath12k_vif_update_vp_config(ahvif, ppe_vp_type);
+
+		if (ret)
+			pr_err("ppe_vp mode config update failed\n");
+		else
+			wdev->ppe_vp_type = ppe_vp_type;
+	}
+
+	pr_info("[%s] vendor cmd type [%s] %d (%s) state %d\n",
+		current->comm,  wdev->netdev->name, wdev->ppe_vp_type,
+		type, netif_running(wdev->netdev));
+	}
+
+	return 0;
+}
+
+static int ath12k_vendor_get_wifi_config_handler(struct wiphy *wiphy,
+						 struct wireless_dev *wdev,
+						 const void *data,
+						 int data_len)
+{
+	struct ieee80211_vif *vif;
+	struct ath12k_vif *ahvif;
+	struct sk_buff *skb;
+	int ret;
+
+	skb = cfg80211_vendor_cmd_alloc_reply_skb(wiphy, NLMSG_DEFAULT_SIZE);
+	if (!skb)
+		return -ENOMEM;
+
+	vif = wdev_to_ieee80211_vif_vlan(wdev, false);
+	if (!vif) {
+		ret = -EINVAL;
+		goto err;
+	}
+
+	ahvif = ath12k_vif_to_ahvif(vif);
+	if (!ahvif) {
+		ret = -EINVAL;
+		goto err;
+	}
+
+//	wiphy_lock(ahvif->ah->hw->wiphy);
+	if (nla_put_u8(skb, QCA_WLAN_VENDOR_ATTR_IF_OFFLOAD_TYPE, ahvif->dp_vif.ppe_vp_type)) {
+		wiphy_unlock(ahvif->ah->hw->wiphy);
+		ret = -EINVAL;
+		goto err;
+	}
+//	wiphy_unlock(ahvif->ah->hw->wiphy);
+
+	ret = cfg80211_vendor_cmd_reply(skb);
+	if (ret) {
+		pr_err("offload type send failed with err=%d\n", ret);
+		return ret;
+	}
+
+	return 0;
+
+err:
+	pr_err("get offload type failed with err=%d\n", ret);
+	kfree_skb(skb);
+	return ret;
+}
+
+static
+int ath12k_vendor_trigg_pri_link_migrate(struct wiphy *wiphy,
+					 struct wireless_dev *wdev,
+					 const void *data, int data_len)
+{
+	struct ieee80211_vif *vif = wdev_to_ieee80211_vif(wdev);
+	struct ath12k_mac_link_migrate_usr_params arg;
+	u8 mac_addr[ETH_ALEN] = {0};
+	struct ath12k_vif *ahvif;
+	u8 link_id;
+	int ret;
+
+	if (WARN_ON(!vif))
+		return -EINVAL;
+
+	/* 1 byte of link ID or(and) 6 bytes of mac address */
+	if (data_len != 1 && data_len != ETH_ALEN + 1)
+		return -EINVAL;
+
+	/* not supported in case of non-ML vif */
+	if (!vif->valid_links)
+		return -EOPNOTSUPP;
+
+	/* get link ID */
+	link_id = *(u8 *)data;
+
+	/* get mac address if it is provided */
+	if (data_len == ETH_ALEN + 1) {
+		data++;
+		memcpy(mac_addr, data, ETH_ALEN);
+	}
+
+	ahvif = (struct ath12k_vif *)vif->drv_priv;
+	if (!ahvif)
+		return -EINVAL;
+
+	ath12k_dbg(NULL, ATH12K_DBG_MAC,
+		   "primary link migration command received link_id %u, mac_addr %pM",
+		   link_id, mac_addr);
+
+	arg.link_id = link_id;
+	memcpy(arg.addr, mac_addr, ETH_ALEN);
+
+	mutex_lock(&ahvif->ah->hw_mutex);
+	ret = ath12k_mac_process_link_migrate_req(ahvif, &arg);
+	mutex_unlock(&ahvif->ah->hw_mutex);
+
+	if (ret)
+		ath12k_info(NULL,
+			    "Failed to trigger primary link migration command\n");
+
+	return ret;
+}
+
+static struct wiphy_vendor_command ath12k_vendor_commands[] = {
+	{
+		.info.vendor_id = QCA_NL80211_VENDOR_ID,
+		.info.subcmd = QCA_NL80211_VENDOR_SUBCMD_SET_WIFI_CONFIGURATION,
+		.doit = ath12k_vendor_wifi_config_handler,
+		.policy = ath12k_wifi_config_policy,
+		.maxattr = QCA_WLAN_VENDOR_ATTR_CONFIG_MAX,
+		.flags = WIPHY_VENDOR_CMD_NEED_NETDEV,
+	},
+	{
+		.info.vendor_id = QCA_NL80211_VENDOR_ID,
+		.info.subcmd = QCA_NL80211_VENDOR_SUBCMD_GET_WIFI_CONFIGURATION,
+		.doit = ath12k_vendor_get_wifi_config_handler,
+		.policy = ath12k_wifi_config_policy,
+		.maxattr = QCA_WLAN_VENDOR_ATTR_CONFIG_MAX,
+		.flags = WIPHY_VENDOR_CMD_NEED_NETDEV,
+	},
+	{
+		.info.vendor_id = QCA_NL80211_VENDOR_ID,
+		.info.subcmd = QCA_NL80211_VENDOR_SUBCMD_PRI_LINK_MIGRATE,
+		.doit = ath12k_vendor_trigg_pri_link_migrate,
+		.policy = VENDOR_CMD_RAW_DATA,
+		.flags = WIPHY_VENDOR_CMD_NEED_NETDEV |
+			 WIPHY_VENDOR_CMD_NEED_RUNNING,
+	},
+};
+
+static const struct nl80211_vendor_cmd_info ath12k_vendor_events[] = {
+};
+
+int ath12k_vendor_register(struct ath12k_hw *ah)
+{
+	ah->hw->wiphy->vendor_commands = ath12k_vendor_commands;
+	ah->hw->wiphy->n_vendor_commands = ARRAY_SIZE(ath12k_vendor_commands);
+	ah->hw->wiphy->vendor_events = ath12k_vendor_events;
+	ah->hw->wiphy->n_vendor_events = ARRAY_SIZE(ath12k_vendor_events);
+	return 0;
+}
