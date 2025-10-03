978827372d683200e9352aeb8de265e18e7cbd0a
Pavankumar Nandeshwar <pnandesh@qti.qualcomm.com>
2025-10-03 10:47:53 +0530

    UPSTREAM:DP:wifi:ath12k: Protect the dp->peers list with dp_lock in recovery path
    Protect the dp->peers list with dp_lock in recovery path while accessing it
    to delete the link peers.
    
    To achieve this, save the mac address and vdev_id in separate per ar tables
    in the first loop with dp_lock held, And in the second loop call the
    dp_peer_delete without holding the dp_lock.
    
    The above split is necessary, as calling the dp_peer_delete in the first loop
    itself will result in deadlock.
    
    patch-dependency:
    0344-0002-QSDK-CP-wifi-ath12k-Add-support-for-Mode-1-recovery-.patch
    
    Change-Id: I87ab0f823dccca8d5d7f9299f001f999399c11e9
    Signed-off-by: Pavankumar Nandeshwar <pnandesh@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
@@ -2110,6 +2110,11 @@ struct ath12k_fw_stats_pdev {
 	s32 mpdu_errs;
 };
 
+struct ar_sta_cookie {
+	u8 addr[ETH_ALEN];
+	int vdev_id;
+};
+
 void ath12k_core_panic_notifier_unregister(struct ath12k_base *ab);
 int ath12k_core_qmi_firmware_ready(struct ath12k_base *ab);
 int ath12k_core_init(struct ath12k_base *ath12k);
