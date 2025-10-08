fbbf3f15d4eaabd112cc46f94e4937a843993848
Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>
2025-10-09 00:13:18 +0530

    UPSTREAM:PROTOCOL: ath12k: set mgmt link-agnostic flag before address translation
    Currently, the link-agnostic flag is never set for action frames since
    its assignment happens after address translation and link selection.
    This causes action frames to miss the link-agnostic marking,
    preventing firmware from performing MLD-level link selection.
    
    Move the link-agnostic flag assignment before address translation
    to ensure action frames are correctly marked as link agnostic.
    The flag may still be cleared later based on action category.
    
    Fixes: Iadecb293d8 ("CP: wifi: ath12k: PKG upgrade changes set 6")
    
    Patch-dependency: NA
    
    Patch-work: https://patch.msgid.link/20250711091704.3704379-1-quic_rdevanat@quicinc.com
    
    Change-Id: I15babd4a54cea19daf0aa38b0ba55f404ecedf66
    Signed-off-by: Hari Naraayana Desikan Kannan <hnaraaya@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
@@ -14918,6 +14918,20 @@ u8 ath12k_mac_get_tx_link(struct ieee80211_sta *sta, struct ieee80211_vif *vif,
 			}
 		}
 	}
+
+	/* Check if this mgmt frame can be queued at MLD level, in that
+	 * case the FW can decide on which link it needs to be finally
+	 * transmitted based on the power state of that link.
+	 * The link param returned by this function still needs
+	 * to be valid to get queued to one of the valid link FW
+	 */
+	if (ath12k_mac_is_mgmt_link_agnostic(skb)) {
+		ATH12K_SKB_CB(skb)->flags |= ATH12K_SKB_MGMT_LINK_AGNOSTIC;
+		/* For action frames this will be reset if not needed
+		 * later based on action category.
+		 */
+	}
+
 	/* Perform address conversion for ML STA Tx */
 	bss_conf = rcu_dereference(vif->link_conf[link]);
 	link_sta = rcu_dereference(sta->link[link]);
@@ -14959,18 +14973,6 @@ u8 ath12k_mac_get_tx_link(struct ieee80211_sta *sta, struct ieee80211_vif *vif,
 			ether_addr_copy(hdr->addr3, bss_conf->addr);
 	}
 
-	/* Check if this mgmt frame can be queued at MLD level, in that
-	 * case the FW can decide on which link it needs to be finally
-	 * transmitted based on the power state of that link.
-	 * The link param returned by this function still needs
-	 * to be valid to get queued to one of the valid link FW
-	 */
-	if (ath12k_mac_is_mgmt_link_agnostic(skb)) {
-		ATH12K_SKB_CB(skb)->flags |= ATH12K_SKB_MGMT_LINK_AGNOSTIC;
-		/* For action frames this will be reset if not needed
-		 * later based on action category.
-		 */
-	}
 
 	return link;
 }
