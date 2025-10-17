CodeLinaro-mirror/qsdk_oss_src_mac80211_wlan-open

commit 74dbd5227e8704734f0451653e9c17362729b08c

mac80211: UPSTREAMDPwifi Fix KW issue rx->sta dereferenced without checking for null condition
In the function __ieee80211_rx_h_amsdu(), the rx->sta pointer is
dereferenced without verifying if it is NULL, even though it can be
NULL as indicated by a previous condition check.

Hence to fix this, check rx->sta is not NULL before proceeding with
the next condition.

patch-dependency: none

Change-Id: I470b2dfd8fe85ad2b2c0ce6d6e0e15b0f65d699c
Signed-off-by: Sarika Sharma <quic_sarishar@quicinc.com>
Signed-off-by: Balamurugan Ramar <quic_bramar@quicinc.com>
Signed-off-by: Balamurugan S <bselvara@qti.qualcomm.com>

backports-6.1-9a0dddfb3/net/mac80211/rx.c
@@ -3183,7 +3183,7 @@ __ieee80211_rx_h_amsdu(struct ieee80211_rx_data *rx, u8 data_offset)
 					  data_offset, true))
 		return RX_DROP_U_BAD_AMSDU;
 
-	if (rx->sta->amsdu_mesh_control < 0) {
+	if (rx->sta && rx->sta->amsdu_mesh_control < 0) {
 		s8 valid = -1;
 		int i;
 

commit f61e7fbda8d98ff4317c3f58c9d57fac72e8d221

QSDK:DP:wifi:ath12k: remove redundant rx->sta check
Currently, in __ieee80211_rx_h_amsdu(), the code checks for rx->sta
before accessing amsdu_mesh_control. However, this check is redundant
because rx->sta is already validated in the caller or the caller's
caller before entering this function.

Hence, remove the unnecessary redundant rx->sta check in
__ieee80211_rx_h_amsdu() to simplify the logic.

Fixes: 74dbd5227e87("mac80211: UPSTREAMDPwifi Fix KW issue rx->sta dereferenced without checking for null condition")
Change-Id: Ice89e3c9cdd5b9762ee94b34aea4767b53ef5334
Signed-off-by: Sarika Sharma <sarika.sharma@oss.qualcomm.com>

backports-6.1-9a0dddfb3/net/mac80211/rx.c
@@ -3482,7 +3482,7 @@ __ieee80211_rx_h_amsdu(struct ieee80211_rx_data *rx, u8 data_offset)
 		return RX_DROP_U_BAD_AMSDU;
 	}
 
-	if (rx->sta && rx->sta->amsdu_mesh_control < 0) {
+	if (rx->sta->amsdu_mesh_control < 0) {
 		s8 valid = -1;
 		int i;
 