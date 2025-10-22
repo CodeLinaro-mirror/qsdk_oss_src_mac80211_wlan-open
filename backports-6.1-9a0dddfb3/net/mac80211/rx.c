e3c766eac580852a3c68a7b2bb04591e21037f0a
Arunpandi Kannan <arunpand@qti.qualcomm.com>
2025-10-22 09:01:19 +0530

    UPSTREAM: CP: wifi:mac80211: Allow rx packets if roc is enabled
    If ROC is enabled, allow rx data flow in mac80211 in STA mode to
    allow DPP related action packets.
    
    Change-Id: I4fe6e37595c3ca6244a8e380a71ec21957b5787d
    Signed-off-by: Arunpandi Kannan <arunpand@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/net/mac80211/rx.c
+++ backports-6.1-9a0dddfb3/net/mac80211/rx.c
@@ -6166,7 +6166,9 @@ static void __ieee80211_rx_handle_packet(struct ieee80211_hw *hw,
 									sts_freq) ||
 					    sdata->vif.is_roc)
 						flag = true;
-				}
+				} else if (sdata->vif.type == NL80211_IFTYPE_STATION &&
+					   sdata->vif.is_roc)
+					flag = true;
 			} else if (sdata->vif.type == NL80211_IFTYPE_STATION &&
 				   sdata->vif.is_roc)
 				flag = true;
@@ -6249,6 +6251,9 @@ static void __ieee80211_rx_handle_packet(struct ieee80211_hw *hw,
 									sts_freq) ||
 					    prev->vif.is_roc)
 						flag = true;
+				} else if (prev->vif.type == NL80211_IFTYPE_STATION &&
+					   prev->vif.is_roc) {
+					flag = true;
 				}
 			} else if (prev->vif.type == NL80211_IFTYPE_STATION &&
 				   prev->vif.is_roc)
