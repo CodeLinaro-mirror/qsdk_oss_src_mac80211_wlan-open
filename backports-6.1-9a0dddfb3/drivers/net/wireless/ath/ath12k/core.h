CodeLinaro-mirror/qsdk_oss_src_mac80211_wlan-open

commit 512188c28025c55f9fa2ad001a59ccb34e09e373

UPSTREAM:CP: wifi:ath12k: Add support for user configured beacon TX rate
Add support for configuring a user defined beacon transmission rate
(legacy rate to EHT rate). This change introduces a new field beacon_tx_rate
in the ath12k_link_vif structure to store the configured rate.
A helper function ath12k_mac_beacon_tx_rate() is implemented
to derive the hardware rate code from the user-provided bitrate mask.

During VDEV start, if a beacon TX rate is specified, it is calculated and
stored in both VDEV start argument and the link VIF structure.
The WMI command is updated to include the beacon TX rate and set the
corresponding flag WMI_VDEV_START_BCN_TX_RATE_PRESENT when applicable.
Additionally, ath12k_recalculate_mgmt_rate() is modified to apply
the first valid basic rate only if the fixed tx rate is not configured for the beacon.

Tested-by: Prasath Rao Loganathan <prasathr@qti.qualcomm.com>
Signed-off-by: Raja Mani <raja.mani@oss.qualcomm.com>

Change-Id: I6e98b3cf223dd7694e1ff4433e722199b34dce6c
Signed-off-by: prasathr <prasathr@qti.qualcomm.com>

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
@@ -579,6 +579,7 @@ struct ath12k_qos_map {
 struct ath12k_link_vif {
 	u32 vdev_id;
 	u32 beacon_interval;
+	u32 beacon_tx_rate;
 	u32 dtim_period;
 
 	struct ath12k *ar;

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
@@ -5319,8 +5319,6 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 	struct ieee80211_hw *hw = ath12k_ar_to_hw(ar);
 	const struct ieee80211_supported_band *sband;
 	struct ieee80211_bss_conf *bss_conf;
-	enum nl80211_band band;
-	u8 beacon_rate_idx;
 	u8 basic_rate_idx;
 	int hw_rate_code;
 	u32 vdev_param;
@@ -5337,7 +5335,6 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 	}
 
 	sband = hw->wiphy->bands[def->chan->band];
-	band = def->chan->band;
 	basic_rate_idx = ffs(bss_conf->basic_rates);
 	if (basic_rate_idx)
 		basic_rate_idx -= 1;
@@ -5354,24 +5351,17 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 					    hw_rate_code);
 	if (ret)
 		ath12k_warn(ar->ab, "failed to set mgmt tx rate %d\n", ret);
-	if (bss_conf->beacon_tx_rate.control[band].legacy) {
-		beacon_rate_idx = ffs(bss_conf->beacon_tx_rate.control[band].legacy);
-		beacon_rate_idx -=1;
 
-		if (band == NL80211_BAND_5GHZ || band == NL80211_BAND_6GHZ)
-			beacon_rate_idx += ATH12K_MAC_FIRST_OFDM_RATE_IDX;
-		if (beacon_rate_idx < ARRAY_SIZE(ath12k_legacy_rates)) {
-			bitrate = ath12k_legacy_rates[beacon_rate_idx].bitrate;
-			hw_rate_code = ath12k_mac_get_rate_hw_value(bitrate);
-
-		}
+	if (!arvif->beacon_tx_rate) {
+		 /* The fixed tx rate is not configured for the beacon,
+		  * apply the first valid basic rate as the beacon tx rate.
+		  */
+		vdev_param = WMI_VDEV_PARAM_BEACON_RATE;
+		ret = ath12k_wmi_vdev_set_param_cmd(ar, arvif->vdev_id, vdev_param,
+						    hw_rate_code);
+		if (ret)
+			ath12k_warn(ar->ab, "failed to set beacon tx rate %d\n", ret);
 	}
-
-	vdev_param = WMI_VDEV_PARAM_BEACON_RATE;
-	ret = ath12k_wmi_vdev_set_param_cmd(ar, arvif->vdev_id, vdev_param,
-					    hw_rate_code);
-	if (ret)
-		ath12k_warn(ar->ab, "failed to set beacon tx rate %d\n", ret);
 }
 
 static void ath12k_update_obss_color_notify_work(struct wiphy *wiphy,
@@ -17762,6 +17752,54 @@ static struct ieee80211_channel *ath12k_mac_get_a_valid_channel(struct ath12k *a
 	return NULL;
 }
 
+#define _bitrate_mask_to_rate_code(_mcs, _mode)					\
+({										\
+	u8 i = 0, nss;								\
+	for (nss = 0; i < ARRAY_SIZE(mask->control[band]._mcs); i++) {		\
+		if (!mask->control[band]._mcs[i])				\
+			continue;						\
+		if (hweight16(mask->control[band]._mcs[i]) == 1) {		\
+			rate_idx = ffs(mask->control[band]._mcs[i]) - 1;	\
+			return ATH12K_HW_RATE_CODE(rate_idx, nss,		\
+						   WMI_RATE_PREAMBLE_##_mode);	\
+		}								\
+	}									\
+})
+
+static u32 ath12k_mac_beacon_tx_rate(struct cfg80211_bitrate_mask *mask,
+				     enum nl80211_band band)
+{
+	u8 preamble, hw_rate, rate_idx;
+	u16 bitrate;
+
+	_bitrate_mask_to_rate_code(eht_mcs, EHT);
+	_bitrate_mask_to_rate_code(he_mcs, HE);
+	_bitrate_mask_to_rate_code(vht_mcs, VHT);
+	_bitrate_mask_to_rate_code(ht_mcs, HT);
+
+	if (hweight32(mask->control[band].legacy) == 1) {
+		rate_idx = ffs(mask->control[band].legacy) - 1;
+
+		if (band == NL80211_BAND_5GHZ || band == NL80211_BAND_6GHZ)
+			rate_idx += ATH12K_MAC_FIRST_OFDM_RATE_IDX;
+
+		if (rate_idx >= ARRAY_SIZE(ath12k_legacy_rates))
+			return 0;
+
+		hw_rate = ath12k_legacy_rates[rate_idx].hw_value;
+		bitrate = ath12k_legacy_rates[rate_idx].bitrate;
+
+		if (ath12k_mac_bitrate_is_cck(bitrate))
+			preamble = WMI_RATE_PREAMBLE_CCK;
+		else
+			preamble = WMI_RATE_PREAMBLE_OFDM;
+
+		return ATH12K_HW_RATE_CODE(hw_rate, 0, preamble);
+	}
+
+	return 0;
+}
+
 static int
 ath12k_mac_vdev_start_restart(struct ath12k_link_vif *arvif,
 			      struct ieee80211_chanctx_conf *ctx,
@@ -17882,6 +17920,12 @@ ath12k_mac_vdev_start_restart(struct ath12k_link_vif *arvif,
 		arg.regdomain = ar->ab->dfs_region;
 		spin_unlock_bh(&ab->base_lock);
 
+		if (chandef && !restart) {
+			arg.bcn_tx_rate =
+				ath12k_mac_beacon_tx_rate(&link_conf->beacon_tx_rate,
+							  chandef->chan->band);
+			arvif->beacon_tx_rate = arg.bcn_tx_rate;
+		}
 		/* TODO: Notify if secondary 80Mhz also needs radar detection */
 	}
 
@@ -22967,6 +23011,11 @@ static int ath12k_mac_hw_register(struct ath12k_hw *ah)
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_ACK_SIGNAL_SUPPORT);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_PROTECTION);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_ADVERTISED_TTLM_OFFLOAD);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_LEGACY);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_HT);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_VHT);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_HE);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_EHT);
 
 	if (test_bit(WMI_TLV_SERVICE_BSS_COLOR_OFFLOAD, ar->ab->wmi_ab.svc_map))
 		 wiphy_ext_feature_set(ar->ah->hw->wiphy,

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.c
@@ -1560,7 +1560,10 @@ int ath12k_wmi_vdev_start(struct ath12k *ar, struct wmi_vdev_start_req_arg *arg,
 						 sizeof(*cmd));
 	cmd->vdev_id = cpu_to_le32(arg->vdev_id);
 	cmd->beacon_interval = cpu_to_le32(arg->bcn_intval);
-	cmd->bcn_tx_rate = cpu_to_le32(arg->bcn_tx_rate);
+	if (arg->bcn_tx_rate) {
+		cmd->bcn_tx_rate = cpu_to_le32(arg->bcn_tx_rate);
+		cmd->flags |= cpu_to_le32(WMI_VDEV_START_BCN_TX_RATE_PRESENT);
+	}
 	cmd->dtim_period = cpu_to_le32(arg->dtim_period);
 	cmd->num_noa_descriptors = cpu_to_le32(arg->num_noa_descriptors);
 	cmd->preferred_rx_streams = cpu_to_le32(arg->pref_rx_streams);

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
@@ -3418,6 +3418,7 @@ struct wmi_delete_all_peer_resp_ev {
 
 #define WMI_VDEV_START_HIDDEN_SSID  BIT(0)
 #define WMI_VDEV_START_PMF_ENABLED  BIT(1)
+#define WMI_VDEV_START_BCN_TX_RATE_PRESENT  BIT(2)
 #define WMI_VDEV_START_LDPC_RX_ENABLED BIT(3)
 #define WMI_VDEV_START_HW_ENCRYPTION_DISABLED BIT(4)
 

commit aa82e662c411c31a3f66e46dc3552d16a7e4dfc7

UPSTREAM:CP:wifi:ath12k: Add support for user configured bcn TX rate
Add support for configuring a user defined
beacon transmission rate (anywhere between
the legacy rate to EHT rate).
A new function ath12k_mac_beacon_tx_rate() is
added to convert the user-specified bitrate mask
into the corresponding hardware rate code.

This functionality is now integrated into the .start_ap()
callback exposed to mac80211. When a beacon TX rate is
specified, it is computed and applied to the firmware using WMI_VDEV_PARAM_BEACON_RATE.

Additionally, ath12k_recalculate_mgmt_rate() has been
updated to apply the first valid basic rate only
if a fixed beacon TX rate is not configured.

Tested-by: Prasath Rao Loganathan <prasathr@qti.qualcomm.com>
Signed-off-by: Raja Mani <raja.mani@oss.qualcomm.com>

Fixes: 512188c28025 ("ath12k: Add support for user configured beacon TX rate")
Patch-dependency: d595dfd19f7b49b5df56b295e679bd3e9ceb5a53 (Revert "UPSTREAM:CP: wifi:ath12k: Add support for user configured beacon TX rate")

Change-Id: I139406bd7dffc015d61b4a8759df2b4c21a2e1ca
Signed-off-by: prasathr <prasathr@qti.qualcomm.com>

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
@@ -579,6 +579,7 @@ struct ath12k_qos_map {
 struct ath12k_link_vif {
 	u32 vdev_id;
 	u32 beacon_interval;
+	u32 beacon_tx_rate;
 	u32 dtim_period;
 
 	struct ath12k *ar;

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
@@ -5318,8 +5318,6 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 	struct ieee80211_hw *hw = ath12k_ar_to_hw(ar);
 	const struct ieee80211_supported_band *sband;
 	struct ieee80211_bss_conf *bss_conf;
-	enum nl80211_band band;
-	u8 beacon_rate_idx;
 	u8 basic_rate_idx;
 	int hw_rate_code;
 	u32 vdev_param;
@@ -5336,7 +5334,6 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 	}
 
 	sband = hw->wiphy->bands[def->chan->band];
-	band = def->chan->band;
 	basic_rate_idx = ffs(bss_conf->basic_rates);
 	if (basic_rate_idx)
 		basic_rate_idx -= 1;
@@ -5353,24 +5350,17 @@ static void ath12k_recalculate_mgmt_rate(struct ath12k *ar,
 					    hw_rate_code);
 	if (ret)
 		ath12k_warn(ar->ab, "failed to set mgmt tx rate %d\n", ret);
-	if (bss_conf->beacon_tx_rate.control[band].legacy) {
-		beacon_rate_idx = ffs(bss_conf->beacon_tx_rate.control[band].legacy);
-		beacon_rate_idx -= 1;
 
-		if (band == NL80211_BAND_5GHZ || band == NL80211_BAND_6GHZ)
-			beacon_rate_idx += ATH12K_MAC_FIRST_OFDM_RATE_IDX;
-		if (beacon_rate_idx < ARRAY_SIZE(ath12k_legacy_rates)) {
-			bitrate = ath12k_legacy_rates[beacon_rate_idx].bitrate;
-			hw_rate_code = ath12k_mac_get_rate_hw_value(bitrate);
-
-		}
+	if (!arvif->beacon_tx_rate) {
+		/* The fixed tx rate is not configured for the beacon,
+		 * apply the first valid basic rate as the beacon tx rate.
+		 */
+		vdev_param = WMI_VDEV_PARAM_BEACON_RATE;
+		ret = ath12k_wmi_vdev_set_param_cmd(ar, arvif->vdev_id, vdev_param,
+						    hw_rate_code);
+		if (ret)
+			ath12k_warn(ar->ab, "failed to set beacon tx rate %d\n", ret);
 	}
-
-	vdev_param = WMI_VDEV_PARAM_BEACON_RATE;
-	ret = ath12k_wmi_vdev_set_param_cmd(ar, arvif->vdev_id, vdev_param,
-					    hw_rate_code);
-	if (ret)
-		ath12k_warn(ar->ab, "failed to set beacon tx rate %d\n", ret);
 }
 
 static void ath12k_update_obss_color_notify_work(struct wiphy *wiphy,
@@ -7794,6 +7784,121 @@ void ath12k_mac_op_link_info_changed(struct ieee80211_hw *hw,
 	ath12k_mac_bss_info_changed(ar, arvif, info, changed);
 }
 EXPORT_SYMBOL(ath12k_mac_op_link_info_changed);
+
+#define _bitrate_mask_to_rate_code(_mcs, _mode)					\
+({										\
+	u8 i = 0, nss;								\
+	for (nss = 0; i < ARRAY_SIZE(mask->control[band]._mcs); i++) {		\
+		if (!mask->control[band]._mcs[i])				\
+			continue;						\
+		if (hweight16(mask->control[band]._mcs[i]) == 1) {		\
+			rate_idx = ffs(mask->control[band]._mcs[i]) - 1;	\
+			return ATH12K_HW_RATE_CODE(rate_idx, nss,		\
+						   WMI_RATE_PREAMBLE_##_mode);	\
+		}								\
+	}									\
+})
+
+static u32 ath12k_mac_beacon_tx_rate(struct cfg80211_bitrate_mask *mask,
+				     enum nl80211_band band)
+{
+	u8 preamble, hw_rate, rate_idx = 0;
+	u16 bitrate;
+
+	if (band >= NUM_NL80211_BANDS)
+		return 0;
+
+	_bitrate_mask_to_rate_code(eht_mcs, EHT);
+	_bitrate_mask_to_rate_code(he_mcs, HE);
+	_bitrate_mask_to_rate_code(vht_mcs, VHT);
+	_bitrate_mask_to_rate_code(ht_mcs, HT);
+
+	if (hweight32(mask->control[band].legacy) == 1) {
+		rate_idx = ffs(mask->control[band].legacy) - 1;
+
+		if (band == NL80211_BAND_5GHZ || band == NL80211_BAND_6GHZ)
+			rate_idx += ATH12K_MAC_FIRST_OFDM_RATE_IDX;
+
+		if (rate_idx >= ARRAY_SIZE(ath12k_legacy_rates))
+			return 0;
+
+		hw_rate = ath12k_legacy_rates[rate_idx].hw_value;
+		bitrate = ath12k_legacy_rates[rate_idx].bitrate;
+
+		if (ath12k_mac_bitrate_is_cck(bitrate))
+			preamble = WMI_RATE_PREAMBLE_CCK;
+		else
+			preamble = WMI_RATE_PREAMBLE_OFDM;
+
+		return ATH12K_HW_RATE_CODE(hw_rate, 0, preamble);
+	}
+
+	return 0;
+}
+
+static int ath12k_mac_config_beacon_tx_rate(struct wiphy *wiphy,
+					    struct ath12k_link_vif *arvif,
+					    struct ieee80211_bss_conf *bss_conf)
+{
+	struct ieee80211_chanctx_conf *conf;
+	struct ath12k *ar = arvif->ar;
+	u32 beacon_tx_rate;
+	int ret;
+
+	conf = wiphy_dereference(wiphy, bss_conf->chanctx_conf);
+	if (!conf || !conf->def.chan) {
+		ath12k_warn(ar->ab,
+			    "failed to get band for vdev %d in bcn rate set\n",
+			    arvif->vdev_id);
+		return -EINVAL;
+	}
+
+	beacon_tx_rate = ath12k_mac_beacon_tx_rate(&bss_conf->beacon_tx_rate,
+						   conf->def.chan->band);
+	if (!beacon_tx_rate)
+		return 0;
+
+	/* Apply the fixed tx rate for the beacon */
+	ret = ath12k_wmi_vdev_set_param_cmd(ar, arvif->vdev_id,
+					    WMI_VDEV_PARAM_BEACON_RATE,
+					    beacon_tx_rate);
+	if (ret) {
+		ath12k_warn(ar->ab,
+			    "failed to set vdev %d beacon tx rate %d\n",
+			    arvif->vdev_id, ret);
+		return ret;
+	}
+
+	arvif->beacon_tx_rate = beacon_tx_rate;
+
+	return 0;
+}
+
+int ath12k_mac_op_start_ap(struct ieee80211_hw *hw,
+			   struct ieee80211_vif *vif,
+			   struct ieee80211_bss_conf *bss_conf)
+{
+	struct ath12k_vif *ahvif = ath12k_vif_to_ahvif(vif);
+	struct wiphy *wiphy = hw->wiphy;
+	struct ath12k_link_vif *arvif;
+	u8 link_id = bss_conf->link_id;
+	int ret;
+
+	lockdep_assert_wiphy(wiphy);
+
+	arvif = wiphy_dereference(wiphy, ahvif->link[link_id]);
+	if (unlikely(!arvif))
+		return -EINVAL;
+
+	ret = ath12k_mac_config_beacon_tx_rate(wiphy, arvif, bss_conf);
+	if (ret)
+		ath12k_warn(arvif->ar->ab,
+			    "failed to configure beacon tx rate for vdev %d: %d\n",
+			    arvif->vdev_id, ret);
+
+	return 0;
+}
+EXPORT_SYMBOL(ath12k_mac_op_start_ap);
 
 static struct ath12k*
 ath12k_mac_select_scan_device(struct ieee80211_hw *hw,
@@ -23034,6 +23139,10 @@ static int ath12k_mac_hw_register(struct ath12k_hw *ah)
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_PUNCT);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_SET_SCAN_DWELL);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_LEGACY);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_HT);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_VHT);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_HE);
+	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_BEACON_RATE_EHT);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_MLD_LINK_REMOVAL_OFFLOAD);
 
 	for_each_ar(ah, ar, i) {

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.h
@@ -316,6 +316,9 @@ void ath12k_mac_op_link_info_changed(struct ieee80211_hw *hw,
 				     struct ieee80211_vif *vif,
 				     struct ieee80211_bss_conf *info,
 				     u64 changed);
+int ath12k_mac_op_start_ap(struct ieee80211_hw *hw,
+			   struct ieee80211_vif *vif,
+			   struct ieee80211_bss_conf *bss_conf);
 void ath12k_mac_op_vif_cfg_changed(struct ieee80211_hw *hw,
 				   struct ieee80211_vif *vif,
 				   u64 changed);

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/hw.c
@@ -1712,6 +1712,7 @@ static const struct ieee80211_ops ath12k_ops_wifi7 = {
 	.config                         = ath12k_mac_op_config,
 	.sta_set_4addr			= ath12k_mac_op_sta_set_4addr,
 	.link_info_changed              = ath12k_mac_op_link_info_changed,
+	.start_ap                       = ath12k_mac_op_start_ap,
 	.vif_cfg_changed		= ath12k_mac_op_vif_cfg_changed,
 	.change_vif_links               = ath12k_mac_op_change_vif_links,
 	.configure_filter		= ath12k_mac_op_configure_filter,