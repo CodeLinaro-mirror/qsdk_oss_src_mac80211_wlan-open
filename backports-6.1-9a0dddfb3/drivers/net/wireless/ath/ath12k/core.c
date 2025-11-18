d27b0cba423a2e8cdc2c7c2e7ee94eb2c71b11c8
Aaradhana Sahu <aaradhana.sahu@oss.qualcomm.com>
2025-11-18 16:29:53 +0530

    UPSTREAM:CP: wifi: ath12k: Set ah->state to ATH12K_HW_STATE_ON after ERP exit when all interfaces are up
    During ERP enter, ah->state is correctly set to ATH12K_HW_STATE_OFF when
    all interfaces are down. However, on ERP exit, ah->state is not updated
    back to ATH12K_HW_STATE_ON even after all interfaces come up.
    Due to this txpower zero in iw dev output.
    
    Update ah->state to ATH12K_HW_STATE_ON after ERP exit when all
    interfaces are up to get proper txpower.
    
    Patch-dependency:
    UPSTREAM:PLATFORM: wifi: ath12k: Add support for powering up Q6
    Patch-work: None
    
    Change-Id: I895bebd536d01c139ad1f3f6db10d96225df19f8
    Signed-off-by: Aaradhana Sahu <aaradhana.sahu@oss.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.c
@@ -2521,6 +2521,7 @@ static void ath12k_core_radio_start(struct ath12k_base *ab)
 {
 	struct ath12k_hw_group *ag = ab->ag;
 	struct ath12k *ar;
+	struct ath12k_hw *ah = ath12k_ag_to_ah(ag, 0);
 	int i, j;
 
 	mutex_lock(&ag->mutex);
@@ -2549,6 +2550,7 @@ static void ath12k_core_radio_start(struct ath12k_base *ab)
 		complete(&ab->power_up);
 	}
 
+	ah->state = ATH12K_HW_STATE_ON;
 	mutex_unlock(&ag->mutex);
 
 	if (test_bit(ATH12K_GROUP_FLAG_HIF_POWER_DOWN, &ag->flags))
