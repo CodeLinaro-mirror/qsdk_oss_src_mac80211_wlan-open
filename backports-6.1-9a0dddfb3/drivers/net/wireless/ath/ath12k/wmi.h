9853982c54c8f3c5d573dc142d4210ce11f8cade
Paulson Raja L <lraja@qti.qualcomm.com>
2025-11-18 22:56:39 +0530

    UPSTREAM:CP:wifi:ath12k: Set high scan priority and dwell times for STA interfaces.
    When a station is configured for multiple links, the 6GHz partner may
    not appear in the initial scan results, causing association with
    fewer links. During parallel scanning, the 6GHz partner is
    discovered later, triggering a disconnect/reconnect loop to add
    the missing link.
    
    To improve discovery of 6GHz partners during initial association, set
    'scan_priority'  to high and increase dwell times for active and
    passive scan.
    
    Patch-dependency: none
    
    Change-Id: I025e2678f283ad67c22a30905a3fa2cf75d5c02b
    Signed-off-by: Ramasamy Kaliappan <rkaliapp@qti.qualcomm.com>
    Signed-off-by: Paulson Raja L <lraja@qti.qualcomm.com>
    (cherry picked from commit 11576087cfe442754fa1bd3a590b3bf7b5cab925)

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
@@ -9049,7 +9049,8 @@ int ath12k_wmi_send_peer_delete_cmd(struct ath12k *ar,
 				    u32 mlo_hw_link_id_bitmap);
 int ath12k_wmi_vdev_delete(struct ath12k *ar, u8 vdev_id);
 void ath12k_wmi_start_scan_init(struct ath12k *ar,
-				struct ath12k_wmi_scan_req_arg *arg);
+				struct ath12k_wmi_scan_req_arg *arg,
+				enum nl80211_iftype vif_type);
 int ath12k_wmi_send_scan_start_cmd(struct ath12k *ar,
 				   struct ath12k_wmi_scan_req_arg *arg);
 int ath12k_wmi_update_scan_chan_list(struct ath12k *ar,
