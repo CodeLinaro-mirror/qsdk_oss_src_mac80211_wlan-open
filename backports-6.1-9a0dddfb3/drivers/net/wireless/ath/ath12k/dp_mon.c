9f691c011c4feaf50b38e8da49b6492190a1bc34
Aniruddha Mishra <anirmish@qti.qualcomm.com>
2025-12-31 11:14:41 +0530

    UPSTREAM:DP:wifi:ath12k:Rename ath12k_wifi7_dp_mon_get_frag_size_by_idx
    ath12k_wifi7_dp_mon_get_frag_size_by_idx API present in ath12k directory.
    API contains suffix wifi7 in it which is incorrect as it will be used
    for wifi8 processing as well.
    
    Renaming ath12k_wifi7_dp_mon_get_frag_size_by_idx to
    ath12k_dp_mon_get_frag_size_by_idx.
    
    Patch-dependency: Iadf8c22d0a5591bcbdfb42932dfa1117573cdcd8 (Upstream: DP: wifi: ath12k:
    Support Ethernet packet capture in monitor mode)
    
    Change-Id: I1d1144843a391836dad25cee33eaca24a9a696ac
    Signed-off-by: Aniruddha Mishra <anirmish@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_mon.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_mon.c
@@ -1976,7 +1976,7 @@ void ath12k_dp_mon_skb_remove_frag(struct ath12k_dp *dp, struct sk_buff *skb,
 	if (unlikely(!page))
 		return;
 
-	frag_len = ath12k_wifi7_dp_mon_get_frag_size_by_idx(dp, skb, idx);
+	frag_len = ath12k_dp_mon_get_frag_size_by_idx(dp, skb, idx);
 	put_page(page);
 	skb->len -= frag_len;
 	skb->data_len -= frag_len;
@@ -2062,8 +2062,8 @@ ath12k_dp_mon_cnt_skb_and_frags(struct sk_buff *skb, u32 *skb_count, u32 *frag_c
 }
 EXPORT_SYMBOL(ath12k_dp_mon_cnt_skb_and_frags);
 
-u32 ath12k_wifi7_dp_mon_get_frag_size_by_idx(struct ath12k_dp *dp,
-					     struct sk_buff *skb, u8 idx)
+u32 ath12k_dp_mon_get_frag_size_by_idx(struct ath12k_dp *dp,
+				       struct sk_buff *skb, u8 idx)
 {
 	u32 size = 0;
 
@@ -2072,7 +2072,7 @@ u32 ath12k_wifi7_dp_mon_get_frag_size_by_idx(struct ath12k_dp *dp,
 
 	return size;
 }
-EXPORT_SYMBOL(ath12k_wifi7_dp_mon_get_frag_size_by_idx);
+EXPORT_SYMBOL(ath12k_dp_mon_get_frag_size_by_idx);
 
 void *ath12k_dp_mon_skb_get_frag_addr(struct sk_buff *skb, u8 idx)
 {
