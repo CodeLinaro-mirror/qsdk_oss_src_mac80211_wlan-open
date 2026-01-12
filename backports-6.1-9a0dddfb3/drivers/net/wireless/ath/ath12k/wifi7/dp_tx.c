CodeLinaro-mirror/qsdk_oss_src_mac80211_wlan-open

commit 34e4b8f8eacffdbd239e4847b86fcde574d2ebf8

UPSTREAM: DP: wifi: ath12k: Enhance TX TID statistics on DS path
This change enables TX TID statistics tracking on the DS path in ath12k DP.
It improves visibility into TX flows, aiding in better debugging and analysis.
Additionally, the TID value assignment logic has been corrected to ensure
accurate and consistent TID values.

Sample output of extended TX TID statistics:

                ath12k TX STATS
TID      packets bytes sfe_pkts multicast  eapol  null_pkts  unicast
null_complete fast_unicast wbm_rel fw_status  ppeds
TID0    26391989 , 41992208  1 :0  2 :12  3 :0  4 :0  5 :13979  6 :0  7
:0  8 :13991  9 :9  10 :26364025
TID1    0 , 0  1 :0  2 :0  3 :0  4 :0  5 :0  6 :0  7 :0  8 :0  9 :0  10
:0
TID2    0 , 0  1 :0  2 :0  3 :0  4 :0  5 :0  6 :0  7 :0  8 :0  9 :0  10
:0

Patch-dependency: I2d39ebd0f4001e7a4b0b6e084d37c69fd0143e9c ("UPSTREAM:DP:wifi:ath12k: Enable VoW support in ath12k driver")

Fixes: e20c41955119 ("UPSTREAM:DP:wifi:ath12k: Enable VoW support in ath12k driver")

Change-Id: Id27354db1908f9c7b2fd426a4e0ea4e2a1f88e3d
Signed-off-by: Girish Kumar M P <gmp@qti.qualcomm.com>

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp_tx.c
@@ -42,26 +42,6 @@ struct ath12k_wifi7_tx_status_entry {
 
 static_assert(sizeof(struct ath12k_wifi7_tx_status_entry) == 64, "size of struct ath12k_wifi7_tx_status_entry is not 64 bytes!");
 
-void ath12k_tid_tx_stats(struct ath12k_vif *ahvif, u8 tid, u32 len, u32 reason)
-{
-	struct pcpu_netdev_tid_stats *tstats = this_cpu_ptr(ahvif->tstats);
-
-	u64_stats_update_begin(&tstats->syncp);
-	tstats->tid_stats[tid].tx_pkt_stats[reason]++;
-	tstats->tid_stats[tid].tx_pkt_bytes[reason] += len;
-	u64_stats_update_end(&tstats->syncp);
-}
-
-void ath12k_tid_tx_drop_stats(struct ath12k_vif *ahvif, u8 tid, u32 len, u32 reason)
-{
-	struct pcpu_netdev_tid_stats *tstats = this_cpu_ptr(ahvif->tstats);
-
-	u64_stats_update_begin(&tstats->syncp);
-	tstats->tid_stats[tid].tx_drop_stats[reason]++;
-	tstats->tid_stats[tid].tx_drop_bytes[reason] += len;
-	u64_stats_update_end(&tstats->syncp);
-}
-
 static enum hal_tcl_encap_type
 ath12k_dp_tx_get_encap_type(struct ath12k_base *ab, struct sk_buff *skb)
 {
@@ -2061,9 +2041,6 @@ static void ath12k_wifi7_dp_tx_complete_msdu(struct ath12k_pdev_dp *dp_pdev,
 			//TODO: Remove this print and add as a stats
 			ath12k_dbg(ab, ATH12K_DBG_DP_TX, "tx frame is not acked status %d\n", ts->status);
  		}
-		if (ath12k_dp_stats_enabled(dp_pdev) &&
-		    ath12k_tid_stats_enabled(dp_pdev))
-			ath12k_tid_tx_drop_stats(ahvif, ts->tid, msdu->len, reason);
 	}
 
 	/* NOTE: Tx rate status reporting. Tx completion status does not have
@@ -2126,10 +2103,13 @@ static void ath12k_wifi7_dp_tx_complete_msdu(struct ath12k_pdev_dp *dp_pdev,
 
 exit:
 	DP_DEVICE_STATS_INC(dp, tx_err.tx_comp_err[drop_reason][ring], 1);
+	if (ahvif && ath12k_dp_stats_enabled(dp_pdev) &&
+	    ath12k_tid_stats_enabled(dp_pdev))
+		ath12k_tid_tx_drop_stats(ahvif, tid, msdu_len, reason);
 	rcu_read_unlock();
 }
 
-static void
+void
 ath12k_wifi7_dp_tx_status_parse(struct ath12k_base *ab,
 				struct hal_wbm_completion_ring_tx *desc,
 				struct hal_tx_status *ts)

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_tx.c
@@ -18,6 +18,28 @@
 #include "dp_stats.h"
 #include "dp_htt.h"
 
+void ath12k_tid_tx_stats(struct ath12k_vif *ahvif, u8 tid, u32 len, u32 reason)
+{
+	struct pcpu_netdev_tid_stats *tstats = this_cpu_ptr(ahvif->tstats);
+
+	u64_stats_update_begin(&tstats->syncp);
+	tstats->tid_stats[tid].tx_pkt_stats[reason]++;
+	tstats->tid_stats[tid].tx_pkt_bytes[reason] += len;
+	u64_stats_update_end(&tstats->syncp);
+}
+EXPORT_SYMBOL(ath12k_tid_tx_stats);
+
+void ath12k_tid_tx_drop_stats(struct ath12k_vif *ahvif, u8 tid, u32 len, u32 reason)
+{
+	struct pcpu_netdev_tid_stats *tstats = this_cpu_ptr(ahvif->tstats);
+
+	u64_stats_update_begin(&tstats->syncp);
+	tstats->tid_stats[tid].tx_drop_stats[reason]++;
+	tstats->tid_stats[tid].tx_drop_bytes[reason] += len;
+	u64_stats_update_end(&tstats->syncp);
+}
+EXPORT_SYMBOL(ath12k_tid_tx_drop_stats);
+
 #ifdef CPTCFG_ATH12K_PPE_DS_SUPPORT
 static void
 ath12k_dp_ppeds_tx_release_desc_list_bulk(struct ath12k_dp *dp,
@@ -166,6 +188,103 @@ void ath12k_hal_srng_ppeds_dst_inv_entry(struct ath12k_base *ab,
 }
 #endif
 
+#ifdef CPTCFG_ATH12K_PPE_DS_SUPPORT
+static void ath12k_ppeds_tx_update_stats(struct ath12k *ar, int skb_len,
+					 struct hal_wbm_completion_ring_tx *tx_status)
+{
+	struct ath12k_base *ab = ar->ab;
+	struct ath12k_dp *dp;
+	struct ath12k_pdev_dp *dp_pdev = &ar->dp;
+	struct ath12k_dp_link_peer *peer;
+	struct ath12k_vif *ahvif;
+	struct ath12k_link_sta *arsta;
+	struct hal_tx_status ts = { 0 };
+	bool tx_drop = false;
+	bool tx_status_default = false;
+	struct ieee80211_tx_info info;
+	u8 reason;
+
+	memset(&info, 0, sizeof(info));
+	info.status.rates[0].idx = -1;
+
+	dp = ath12k_ab_to_dp(ab);
+	dp->arch_ops->dp_tx_status_parse(ab, tx_status, &ts);
+	info.status.ack_signal = ATH12K_DEFAULT_NOISE_FLOOR + ts.ack_rssi;
+	info.status.flags = IEEE80211_TX_STATUS_ACK_SIGNAL_VALID;
+	dp->ppe.ppeds_stats.tqm_rel_reason[ts.status]++;
+
+	if (ts.status == HAL_WBM_TQM_REL_REASON_FRAME_ACKED)
+		info.flags |= IEEE80211_TX_STAT_ACK;
+	else if (ts.status == HAL_WBM_TQM_REL_REASON_CMD_REMOVE_TX)
+		info.flags |= IEEE80211_TX_STAT_NOACK_TRANSMITTED;
+
+	if (ts.status != HAL_WBM_TQM_REL_REASON_FRAME_ACKED) {
+		switch (ts.status) {
+		case HAL_WBM_TQM_REL_REASON_CMD_REMOVE_MPDU:
+			reason = ATH_TX_DS_TQM_REMOVE_MPDU;
+			break;
+		case HAL_WBM_TQM_REL_REASON_DROP_THRESHOLD:
+			reason = ATH_TX_DS_TQM_DROP_THRESHOLD;
+			break;
+		case HAL_WBM_TQM_REL_REASON_CMD_REMOVE_TX:
+			reason = ATH_TX_DS_TQM_REMOVE_TX;
+			break;
+		case HAL_WBM_TQM_REL_REASON_CMD_REMOVE_AGED_FRAMES:
+			reason = ATH_TX_DS_TQM_REMOVE_AGED;
+			break;
+		default:
+			reason = ATH_TX_DS_TQM_REMOVE_DEF;
+			//TODO: Remove this print and add as a stats
+			ath12k_dbg(ab, ATH12K_DBG_DP_TX,
+				   "tx frame is not acked status %d\n",
+				   ts.status);
+			tx_status_default = true;
+		}
+		tx_drop = true;
+	}
+
+	rcu_read_lock();
+
+	peer = ath12k_dp_link_peer_find_by_id(dp, ts.peer_id);
+	if (unlikely(!peer || !peer->sta || !peer->vif)) {
+		rcu_read_unlock();
+		return;
+	}
+
+	arsta = ath12k_peer_get_link_sta(ab, peer);
+	if (!arsta) {
+		rcu_read_unlock();
+		return;
+	}
+
+	if (ath12k_dp_stats_enabled(dp_pdev) &&
+	    ath12k_tid_stats_enabled(dp_pdev)) {
+		ahvif = ath12k_vif_to_ahvif(peer->vif);
+		if (tx_drop) {
+			ath12k_tid_tx_drop_stats(ahvif, ts.tid, skb_len,
+						 reason);
+		} else {
+			ath12k_tid_tx_stats(ahvif, ts.tid, skb_len,
+					    ATH_TX_PPEDS_PKTS);
+			ath12k_tid_tx_stats(ahvif, ts.tid, skb_len,
+					    ATH_TX_COMPLETED_PKTS);
+		}
+	}
+
+	if (ts.status != HAL_WBM_TQM_REL_REASON_FRAME_ACKED &&
+	    !tx_status_default) {
+		rcu_read_unlock();
+		return;
+	}
+
+#ifdef CPTCFG_MAC80211_DS_SUPPORT
+	ieee80211_ppeds_tx_update_stats(ar->ah->hw, peer->sta, &info,
+					peer->txrate, peer->link_id, skb_len);
+#endif
+	rcu_read_unlock();
+}
+#endif
+
 u16 dp_sawf_msduq_peer_id_set(u16 peer_id, u8 msduq)
 {
 	u16 peer_msduq = 0;
@@ -300,6 +419,8 @@ void ath12k_ppeds_reinject_handler(struct ath12k_base *ab,
 int ath12k_ppeds_tx_completion_handler(struct ath12k_base *ab, int budget)
 {
 	struct ath12k_dp *dp = ab->dp;
+	struct ath12k *ar;
+	struct ath12k_pdev_dp *dp_pdev;
 	struct dp_ppeds_tx_comp_ring *tx_ring = &dp->ppe.ppeds_comp_ring;
 	int hal_ring_id = tx_ring->ppe_wbm2sw_ring.ring_id;
 	struct hal_srng *status_ring = &ab->hal.srng_list[hal_ring_id];
@@ -307,7 +428,7 @@ int ath12k_ppeds_tx_completion_handler(struct ath12k_base *ab, int budget)
 	int valid_entries, count = 0;
 	int list_no_skb_count = 0;
 	struct hal_wbm_release_ring *desc;
-	struct hal_wbm_completion_ring_tx *tx_status;
+	struct hal_wbm_completion_ring_tx *tx_status, *status;
 	struct htt_tx_wbm_completion *status_desc;
 	enum hal_wbm_rel_src_module buf_rel_source;
 	int htt_status;
@@ -380,6 +501,16 @@ int ath12k_ppeds_tx_completion_handler(struct ath12k_base *ab, int budget)
 		tx_desc->in_use = false;
 		if (likely(tx_desc->skb)) {
 			list_add_tail(&tx_desc->list, &local_list);
+			if (tx_ring->macid[count] != 0xF) {
+				ar = ab->pdevs[tx_ring->macid[count]].ar;
+				dp_pdev = &ar->dp;
+				if (ath12k_dp_stats_enabled(dp_pdev)) {
+					status = &tx_ring->tx_status[count];
+					ath12k_ppeds_tx_update_stats(ar,
+								     tx_desc->skb->len,
+								     status);
+				}
+			}
 			count++;
 		} else {
 			list_add_tail(&tx_desc->list, &local_list_no_skb);

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp.h
@@ -503,6 +503,9 @@ struct ath12k_dp_arch_ops {
 				     struct ath12k_link_vif *arvif,
 				     struct sk_buff *skb, struct ath12k_link_sta *arsta);
 	int (*dp_tx_ring_setup)(struct ath12k_base *ab);
+	void (*dp_tx_status_parse)(struct ath12k_base *ab,
+				   struct hal_wbm_completion_ring_tx *desc,
+				   struct hal_tx_status *ts);
 };
 
 struct ath12k_bp_stats {

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_rx.c
@@ -1020,7 +1020,6 @@ void ath12k_dp_rx_deliver_msdu(struct ath12k_pdev_dp *dp_pdev,
 	struct ieee80211_rx_status *rx_status;
 	struct ieee80211_sta *pubsta = NULL;
 	struct ath12k_dp_peer *peer = NULL;
-	struct ath12k_skb_rxcb *rxcb = ATH12K_SKB_RXCB(msdu);
 	struct ath12k_dp_link_peer *link_peer = NULL;
 	struct ath12k_vif *ahvif;
 	u8 addr[ETH_ALEN] = {0};
@@ -1056,10 +1055,10 @@ void ath12k_dp_rx_deliver_msdu(struct ath12k_pdev_dp *dp_pdev,
 								     peer_id);
 		if (link_peer) {
 			ahvif = ath12k_vif_to_ahvif(link_peer->vif);
-			ath12k_tid_rx_stats(ahvif, rxcb->tid, msdu->len,
+			ath12k_tid_rx_stats(ahvif, tid, msdu->len,
 					    ATH_RX_TOTAL_OUT_PKTS);
 		}
-		msdu->priority = rxcb->tid;
+		msdu->priority = tid;
 	}
 
 	rcu_read_unlock();

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp.c
@@ -344,6 +344,7 @@ static struct ath12k_dp_arch_ops ath12k_wifi7_dp_arch_ops = {
 	.peer_migrate_reo_cmd = ath12k_wifi7_dp_peer_migrate_reo_cmd,
 	.sdwf_reinject_handler = ath12k_wifi7_sdwf_reinject_handler,
 	.dp_tx_ring_setup = ath12k_wifi7_dp_tx_ring_setup,
+	.dp_tx_status_parse = ath12k_wifi7_dp_tx_status_parse,
 };
 
 /* TODO: remove export once this file is built with wifi7 ko */

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp_tx.h
@@ -27,6 +27,9 @@ bool ath12k_mac_tx_check_max_limit(struct ath12k_pdev_dp *dp_pdev, struct sk_buf
 int ath12k_wifi7_sdwf_reinject_handler(struct ath12k_pdev_dp *dp_pdev,
 				       struct ath12k_link_vif *arvif,
 				       struct sk_buff *skb, struct ath12k_link_sta *arsta);
+void ath12k_wifi7_dp_tx_status_parse(struct ath12k_base *ab,
+				     struct hal_wbm_completion_ring_tx *desc,
+				     struct hal_tx_status *ts);
 int ath12k_wifi7_dp_tx_ring_setup(struct ath12k_base *ab);
 void ath12k_wifi7_dp_tx_ring_cleanup(struct ath12k_base *ab);
 #endif

backports-6.1-9a0dddfb3/net/mac80211/rx.c
@@ -5825,6 +5825,7 @@ static void __ieee80211_rx_handle_8023(struct ieee80211_hw *hw,
 	rx.list = list;
 	rx.link_id = -1;
 
+	status->tid = skb->priority & IEEE80211_QOS_CTL_TAG1D_MASK;
 	I802_DEBUG_INC(local->dot11ReceivedFragmentCount);
 
 	/* drop frame if too short for header */
@@ -5944,12 +5945,18 @@ static void __ieee80211_rx_handle_packet(struct ieee80211_hw *hw,
 	bool tid_stats_disable = local->hw.tid_stats_disable;
 	bool prev_flag, is_mgmt = false;
 
+	hdr = (struct ieee80211_hdr *)skb->data;
 	fc = ((struct ieee80211_hdr *)skb->data)->frame_control;
 	memset(&rx, 0, sizeof(rx));
 	rx.skb = skb;
 	rx.local = local;
 	rx.list = list;
 	rx.link_id = -1;
+
+	if (ieee80211_is_data_qos(fc))
+		status->tid = ieee80211_get_tid(hdr);
+	else
+		status->tid = skb->priority & IEEE80211_QOS_CTL_TAG1D_MASK;
 
 	if (ieee80211_is_data(fc) || ieee80211_is_mgmt(fc))
 		I802_DEBUG_INC(local->dot11ReceivedFragmentCount);

commit 5920466cc87f56d9636f36cb2162b5e371fbfc43

QSDK:DP:wifi:ath12k: Correct per-link station dump TX stats
TX stats were being attributed to the primary link during TX completion,
leading to inaccurate per-link metrics in station dump reports.

Fix it by fetching the corresponding link ID using ath12k_dp_get_link_id
and update the stats,

Patch-dependency: Id27354db ("UPSTREAM:DP:wifi:ath12k:Enhance TX TID statistics on DS path")
Fixes: 34e4b8f8eacf ("UPSTREAM:DP:wifi:ath12k:Enhance TX TID statistics on DS path")

Change-Id: I165989b665e31da3d92f7abe9e7cbf1bb5fc10d6
Signed-off-by: Girish Kumar M P <gmp@qti.qualcomm.com>

backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp_tx.c
@@ -2548,7 +2548,7 @@ void ath12k_ppeds_tx_update_stats(struct ath12k *ar, int skb_len,
 	bool tx_drop = false;
 	bool tx_status_default = false;
 	struct ieee80211_tx_info info;
-	u8 reason;
+	u8 reason, link_id;
 
 	memset(&info, 0, sizeof(info));
 	info.status.rates[0].idx = -1;
@@ -2623,9 +2623,11 @@ void ath12k_ppeds_tx_update_stats(struct ath12k *ar, int skb_len,
 		return;
 	}
 
+	link_id = ath12k_dp_get_link_id(dp_pdev, ts.hw_link_id, peer->dp_peer);
+
 #ifdef CPTCFG_MAC80211_DS_SUPPORT
 	ieee80211_ppeds_tx_update_stats(ar->ah->hw, peer->sta, &info,
-					peer->txrate, peer->link_id, 0);
+					peer->txrate, link_id, 0);
 #endif
 	rcu_read_unlock();
 }