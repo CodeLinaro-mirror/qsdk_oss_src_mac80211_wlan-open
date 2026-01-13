0ca4382ae0926b76e2f60d6c699595bc3f270740
Pooventhiran G <pooventh@qti.qualcomm.com>
2026-01-13 18:46:10 +0530

    UPSTREAM: CP: wifi: ath12k: Process management rx frames via new SRNG path
    Process the incoming management frames via the new SRNG path from
    RX_MGMT ring. While reaping the frames from the ring, if a frame is
    split into multiple buffers received from Hardware, stitch them together
    so that a complete frame can be processed further. Once done, forward
    the frames to mac80211. This commit also adds rx descriptor extraction
    logic to replenish used ones after reaping frames from the rings.
    
    Patch-dependency: I97cd850d2a7e ("UPSTREAM: DP: wifi: ath12k: Add util functions for crypto and peer")
    Tag: WiFi8_Mgmt_TxRx
    
    Change-Id: Id57ee2232b5962311969ab1a56c886de3cc0b010
    Signed-off-by: Pooventhiran G <pooventh@qti.qualcomm.com>
    (cherry picked from commit df75396c0cba1dc0578b3f2f000f2f71528579a8)

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mgmt_rx.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mgmt_rx.h
@@ -114,6 +114,8 @@ static inline u32 ath12k_mgmt_gen_rx_desc_cookie(u16 block, u16 slot)
 	return (u32)block << MGMT_RX_DESC_COOKIE_SHIFT | slot;
 }
 
+struct ath12k_rx_desc_info *ath12k_mgmt_get_rx_desc_from_cookie(struct ath12k_mgmt *mgmt,
+								u32 cookie);
 int ath12k_mgmt_rx_desc_init(struct ath12k_base *ab);
 void ath12k_mgmt_rx_desc_cleanup(struct ath12k_base *ab);
 size_t ath12k_mgmt_rx_desc_list_cut_nodes(struct list_head *used_list,
@@ -122,6 +124,8 @@ size_t ath12k_mgmt_rx_desc_list_cut_nodes(struct list_head *used_list,
 size_t ath12k_mgmt_get_req_entries_from_refill_ring(struct ath12k_base *ab,
 						    struct mgmt_srng *rx_refill_ring,
 						    struct list_head *list);
+struct sk_buff *ath12k_mgmt_rx_get_mmpdu_last_buf(struct sk_buff_head *mmpdu_list,
+						  struct sk_buff *first);
 
 static inline int ath12k_mgmt_arch_op_device_init(struct ath12k_mgmt *mgmt)
 {
