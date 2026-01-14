01bd8d1a5f2875a84a506a586c7e45fdfebaa293
Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>
2026-01-14 12:20:55 +0530

    UPSTREAM:PROTOCOL: wifi: ath12k: add UHR PHY modes
    Add support to retrieve and configure the phy modes supported
    by the hardware.
    
    Assign phymode based on the maximum supported mode by hardware.
    
    Tested-on: QCN9625 hw1.0 PCI WLAN.WBO.0.0-01246-QCAHKSWPL_CORE_TO-1
    Patch-dependency: None
    Patch-work: None
    
    Change-Id: I5d69905869a9a9ada4fd21bd324a838198b42bff
    Signed-off-by: Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.c
@@ -1499,7 +1499,7 @@ static void ath12k_wmi_put_wmi_channel(struct ath12k_wmi_channel_params *chan,
 
 	chan->mhz = cpu_to_le32(arg->freq);
 	chan->band_center_freq1 = cpu_to_le32(arg->band_center_freq1);
-	if (arg->mode == MODE_11BE_EHT320) {
+	if (arg->mode == MODE_11BN_UHR320 || arg->mode == MODE_11BE_EHT320) {
 		if (arg->freq > center_freq1)
 			chan->band_center_freq1 =
 					cpu_to_le32(center_freq1 + 80);
@@ -1508,7 +1508,8 @@ static void ath12k_wmi_put_wmi_channel(struct ath12k_wmi_channel_params *chan,
 					cpu_to_le32(center_freq1 - 80);
 
 		chan->band_center_freq2 = cpu_to_le32(arg->band_center_freq1);
-	} else if (arg->mode == MODE_11BE_EHT160) {
+	} else if (arg->mode == MODE_11BN_UHR160 ||
+		   arg->mode == MODE_11BE_EHT160) {
 		if (arg->freq > center_freq1)
 			chan->band_center_freq1 =
 					cpu_to_le32(center_freq1 + 40);
