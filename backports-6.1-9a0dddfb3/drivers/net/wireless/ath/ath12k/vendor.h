eeab42bd295c2fc84e136a5c68b19670fd9f8bda
Nidhi Jain <nidhjain@qti.qualcomm.com>
2026-02-01 01:14:55 +0530

    QSDK:CP:ath12k: Add DCS SIM vendor attribute for OBSS handling
    Add new vendor attribute "QCA_WLAN_VENDOR_ATTR_DCS_SIM_INTERFERENCE_BITMAP"
    for OBSS handling through DCS simulator. It is used to accept channel
    bandwidth interference bitmap from userspace and handle interference
    simulation accordingly.
    
    Patch-dependency: None
    
    Change-Id: I7f1bc742f241eac4d50b37f642e1e8b1ceb329a1
    Signed-off-by: Nidhi Jain <nidhjain@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/vendor.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/vendor.h
@@ -3904,11 +3904,15 @@ enum qca_wlan_vendor_attr_mon_scan_stats {
  * type. Selects the interference simulation mode the driver should execute.
  * Userspace must provide this attribute. QCA_WLAN_VENDOR_ATTR_DCS_SIM_LINK_ID
  * is also required in multi-link AP scenarios.
+ *
+ * @QCA_WLAN_VENDOR_ATTR_DCS_SIM_INTERFERENCE_BITMAP: 32-bit bitmap to get the
+ * channel bandwidth interference bitmap value from userspace.
  */
 enum qca_wlan_vendor_attr_dcs_sim {
 	QCA_WLAN_VENDOR_ATTR_DCS_SIM_INVALID = 0,
 	QCA_WLAN_VENDOR_ATTR_DCS_SIM_LINK_ID,
 	QCA_WLAN_VENDOR_ATTR_DCS_SIM_TYPE,
+	QCA_WLAN_VENDOR_ATTR_DCS_SIM_INTERFERENCE_BITMAP,
 
 	QCA_WLAN_VENDOR_ATTR_DCS_SIM_AFTER_LAST,
 	QCA_WLAN_VENDOR_ATTR_DCS_SIM_MAX =
