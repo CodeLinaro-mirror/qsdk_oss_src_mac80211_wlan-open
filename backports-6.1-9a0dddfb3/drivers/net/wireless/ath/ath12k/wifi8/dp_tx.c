17b0e53cc402e923a3a228dd9f26499525308029
Nandha Kishore Easwaran <nandhaki@qti.qualcomm.com>
2026-02-10 22:08:54 +0530

    UPSTREAM:DP:wifi:ath12k: Add 11BN case in tx completion
    Add 11BN case in Tx completion handling.
    
    Patch-dependency: None
    
    Change-Id: I3f21614a2f3ce7c3740ea73c29440645606853c4
    Signed-off-by: Nandha Kishore Easwaran <nandhaki@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/dp_tx.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/dp_tx.c
@@ -2097,6 +2097,8 @@ ath12k_wifi8_dp_tx_update_txcompl(struct ath12k_pdev_dp *dp_pdev,
 		txrate.flags = RATE_INFO_FLAGS_EHT_MCS;
 		txrate.eht_gi = ath12k_mac_eht_gi_to_nl80211_eht_gi(ts->sgi);
 		break;
+	case HAL_TX_RATE_STATS_PKT_TYPE_11BN:
+		return;
 	default:
 		ath12k_warn(ab, "Invalid tx pkt type: %d\n", ts->pkt_type);
 		return;
