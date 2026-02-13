cb24f1e2e1dd913c39b4623094caaa3360b9a592
Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>
2026-02-13 10:52:50 +0530

    UPSTREAM:PROTOCOL: Revert "UPSTREAM:PROTOCOL: wifi: mac80211: Add UHR capabilities to association/probe request for STA mode"
    This reverts commit ebe5958191a6ab9627bf927ed83933c0a39c941a.
    
    Patch-dependency: None
    
    Change-Id: Iea8ec76337f7d542a2faf3197f1893a3aeb4a3b1
    Signed-off-by: Karthikeyan Kathirvel <karthikeyan.kathirvel@oss.qualcomm.com>

--- backports-6.1-9a0dddfb3/net/mac80211/ieee80211_i.h
+++ backports-6.1-9a0dddfb3/net/mac80211/ieee80211_i.h
@@ -420,7 +420,6 @@ enum ieee80211_conn_mode {
 	IEEE80211_CONN_MODE_VHT,
 	IEEE80211_CONN_MODE_HE,
 	IEEE80211_CONN_MODE_EHT,
-	IEEE80211_CONN_MODE_UHR,
 };
 
 #define IEEE80211_CONN_MODE_HIGHEST	IEEE80211_CONN_MODE_UHR
@@ -2881,9 +2880,7 @@ int ieee80211_put_eht_cap(struct sk_buff *skb,
 			  struct ieee80211_sub_if_data *sdata,
 			  const struct ieee80211_supported_band *sband,
 			  const struct ieee80211_conn_settings *conn);
-int ieee80211_put_uhr_cap(struct sk_buff *skb,
-			  struct ieee80211_sub_if_data *sdata,
-			  const struct ieee80211_supported_band *sband);
+
 /* channel management */
 bool ieee80211_chandef_ht_oper(const struct ieee80211_ht_operation *ht_oper,
 			       struct cfg80211_chan_def *chandef);
