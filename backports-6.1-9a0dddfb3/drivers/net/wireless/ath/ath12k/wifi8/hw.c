844e6c10786d0fde7c74b5a36437922f27822ae9
Madhavan Ganesan <madhgane@qti.qualcomm.com>
2026-03-03 00:26:14 +0530

    UPSTREAM:PLATFORM:wifi:ath12k: Add unified firmware support for Wi-Fi 8
    Wi-Fi 7 platforms use separate firmware images - amss.bin for single MAC
    and amss_dualmac.bin for dual MAC. The driver selects firmware based on
    board ID bit 12 (OTP_VALID_DUALMAC_BOARD_ID_MASK).
    
    Wi-Fi 8 platforms use a unified amss.bin that supports both single‑MAC
    and dual‑MAC configurations.
    
    For Wi‑Fi 8, the ath12k driver writes the board ID to the
    PCIE_PCIE_LOCAL_REG_PCIE_LOCAL_RSV1 register before Q6 bring‑up. The
    firmware reads the board ID from this register to determine the MAC
    configurations.
    
    The ath12k driver updates the board ID from the firmware target
    capability QMI response if a valid board ID is provided; otherwise, it
    falls back to the value from the DTS file.
    
    - Wi-Fi 7: unified_fw_image = false, select firmware based on dualmac
      flag.
    - Wi-Fi 8: unified_fw_image = true, write board ID to PCIe RSV1
      register.
    
    This maintains WiFi7 compatibility while enabling Wi-Fi 8 unified
    firmware. Future Wi-Fi 8 targets only need to set the unified_fw_image
    flag.
    
    Patch-dependency: None
    
    Change-Id: I5c336fb07c4e53c4be37895307f138795164ea97
    Signed-off-by: Madhavan Ganesan <madhgane@qti.qualcomm.com>
    (cherry picked from commit 8fc01d2b1af998e71d10142ebf0c2e0c6d70bc78)

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.c
@@ -226,6 +226,7 @@ static struct ath12k_hw_params ath12k_wifi8_hw_params[] = {
 			.cal_offset = 128 * 1024,
 			.m3_loader = ath12k_m3_fw_loader_driver,
 			.std_elf_img = true,
+			.unified_fw_image = true,
 		},
 		.max_radios = 2,
 		.single_pdev_only = false,
