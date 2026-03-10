0ce79f415ea3d4fcb434ec7912aa2918c8ab7e2d
Nandha Kishore Easwaran <nandhaki@qti.qualcomm.com>
2026-03-10 21:30:38 +0530

    UPSTREAM:DP:wifi:ath12k: set IRQ affinity for ext IRQ groups
    Add a static CPU affinity table for the ext IRQ groups and apply it
    after enabling the interrupts.
    
    Patch-dependency: I5c6c74646887b170d34bb1098f919feedcb1df9b
    ("UPSTREAM:DP:wifi:ath12k: Increase number of interrupts for wifi8")
    
    Change-Id: I327098afbe23dc41cf1eb71b334bbdad4c2e01d3
    Signed-off-by: Nandha Kishore Easwaran <nandhaki@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.h
@@ -43,6 +43,22 @@
 #define ATH12K_REO2PPE_RING_MASK_0 0x1
 #define ATH12K_PPE_WBM2SW_RELEASE_RING_MASK_0 0x1
 
+static const int ath12k_wifi8_ext_irq_grp_affinity[] = {
+	0, /* grp0  -> cpu0 */
+	1, /* grp1  -> cpu1 */
+	2, /* grp2  -> cpu2 */
+	3, /* grp3  -> cpu3 */
+	0, /* grp4  -> cpu0 */
+	1, /* grp5  -> cpu1 */
+	2, /* grp6  -> cpu2 */
+	3, /* grp7  -> cpu3 */
+	0, /* grp8  -> cpu0 */
+	1, /* grp9  -> cpu1 */
+	0, /* grp10 -> cpu0 */
+	1, /* grp11 -> cpu1 */
+	2, /* grp12 -> cpu2 */
+};
+
 struct ath12k_base;
 int ath12k_wifi8_hw_init(struct ath12k_base *ab);
 
