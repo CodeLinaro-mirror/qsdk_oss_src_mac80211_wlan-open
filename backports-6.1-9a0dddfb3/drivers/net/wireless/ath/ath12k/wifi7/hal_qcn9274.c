ef1e71a7fea37fefc39189125f62a7ee04eca713
Pavankumar Nandeshwar <pnandesh@qti.qualcomm.com>
2026-03-11 12:14:33 +0530

    UPSTREAM:DP:wifi:ath12k: Add CCE metadata field support
    Add support for CCE metadata field in the RX descriptor
    structure. This change introduces:
    
    - New hal_ops callback rx_desc_get_cce_metadata() to retrieve CCE
      metadata from RX descriptors
    - Implementation for QCN9274 platform to extract cce_metadata from
      msdu_end structure
    - Modified rx_msdu_end_qcn9274_compact structure to include cce_metadata
      field (16 bits) by splitting the previously reserved field
    
    The CCE metadata is used for packet classification and routing decisions
    in the receive path, particularly for ERP (Error Recovery Procedure)
    feature support.
    
    Patch-dependency:I691a992a31847011a518d4e0e3eae980623e8123
    (UD RX refactoring and cleanup)
    
    Change-Id: I7d6273034ec4e48318a3dec0f1811023d8bbfd60
    Signed-off-by: Pavankumar Nandeshwar <pnandesh@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/hal_qcn9274.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/hal_qcn9274.c
@@ -1145,6 +1145,7 @@ const struct hal_ops hal_qcn9274_ops = {
 	.hal_mon_ops_init = ath12k_wifi7_hal_mon_ops_init,
 	.get_hw_hptp = ath12k_wifi7_hal_get_hw_hptp,
 	.rx_desc_get_fse_info = ath12k_wifi7_hal_rx_desc_get_fse_info_qcn9274,
+	.rx_desc_get_cce_metadata = ath12k_wifi7_hal_rx_desc_get_cce_metadata_qcn9274,
 #ifdef CPTCFG_EXT_IPA_OFFLOAD
 	.reo_hw_setup_ipa = ath12k_hal_reo_ring_ipa_ctrl_hash_ix0_setup,
 #endif
