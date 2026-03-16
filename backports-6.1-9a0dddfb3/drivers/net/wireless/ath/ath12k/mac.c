638e2280384d46848629c5ce7691b8dcecd97ee5
Rhythm Patwa <rpatwa@qti.qualcomm.com>
2026-03-15 20:38:06 -0700

    QSDK:CP:wifi:ath12k: Update the mesh peer assoc WMI flags
    Make changes to indicate to the FW during peer assoc send for a mesh
    peer to not expect a 4-way handshake in secure mode connection. Since
    there is no OTA exchange for peer association, keys are programmed via
    cli for the mesh peers.
    
    Patch-dependency: Ie0ec89ef05a904e6802614d5dd54490971559dad
    [QSDK:CP:wifi:ath12k: Changes to handle mesh configuration commands]
    
    Change-Id: I630e35f1e137064cff8e6ae7d2c8591b8a145afc
    Signed-off-by: Rhythm Patwa <rpatwa@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/mac.c
@@ -5411,6 +5411,9 @@ void ath12k_peer_assoc_prepare(struct ath12k *ar,
 	ath12k_peer_assoc_h_ttlm(arsta, arg);
 	ath12k_peer_assoc_h_flowq(arsta, arvif, arg);
 	ath12k_peer_assoc_h_holq(arsta, arvif, arg);
+#ifdef CPTCFG_QCN_EXTN_MESH_SUPPORT
+	ath12k_peer_assoc_h_auth_extn(arvif, arsta, arg);
+#endif
 
 	arsta->peer_nss = arg->peer_nss;
 
@@ -5420,7 +5423,7 @@ void ath12k_peer_assoc_prepare(struct ath12k *ar,
 
 	/* TODO: amsdu_disable req? */
 }
-#ifdef CPTCFG_QCN_EXTN
+#ifdef CPTCFG_QCN_EXTN_MESH_SUPPORT
 EXPORT_SYMBOL(ath12k_peer_assoc_prepare);
 #endif
 
