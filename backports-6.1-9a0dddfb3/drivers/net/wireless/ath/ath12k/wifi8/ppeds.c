2edf3d6ae2353503dfdec6dc270a9c217b683edf
syed touqeer pasha <spasha@qti.qualcomm.com>
2026-04-03 19:06:58 +0530

    QSDK:DP:wifi:ath12k: Add check for PPEDS registration
    Return if PPEDS is not registered during ppeds stop
    
    Patch-dependency: none
    
    Change-Id: I8e1567d1ee25d554b9260f3ae125136a03c7cf68
    Signed-off-by: syed touqeer pasha <spasha@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/ppeds.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/ppeds.c
@@ -868,6 +868,7 @@ void ath12k_wifi8_dp_ppeds_stop(struct ath12k_base *ab)
 	if (!ab->dp->ppe.nss_plugin_ops) {
 		ath12k_err(ab, "nss_plugin_ops not registered for device_id %d\n",
 			   ab->device_id);
+		return;
 	}
 
 	umac_reset_in_progress = ath12k_dp_umac_reset_in_progress(ab);
