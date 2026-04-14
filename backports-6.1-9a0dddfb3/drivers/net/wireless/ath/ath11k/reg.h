6094186c713181aa13f025bf8f78ceb54c2c9926
Hariharan Basuthkar <hbasuthk@qti.qualcomm.com>
2026-04-14 15:20:59 +0530

    UPSTREAM:CP:wifi:ath11k: remove intersection support for regulatory rules
    Currently, regulatory rules from new country settings is intersected with
    rules from default country settings(during initialisation) in order to
    prevent users to bypass their default country settings such as power
    limits, channel flags, etc.
    
    However, the country setting in the BDF will take higher precedence and
    FW will protect it. Therefore, there is no need to handle intersection
    on the driver side now.
    
    Remove regulatory rules intersection logic support.
    
    This is a port of ath12k commit b3d868c5317d
    ("wifi: ath12k: remove intersection support for regulatory rules").
    
    Patch-dependency: none
    Tag: REGULATORY
    Link - [PATCH ath-next] wifi: ath11k: remove intersection support for regulatory rules
    
    Change-Id: Iae4737054d8c00e9772fc5c0f315ba5352b53f68
    Signed-off-by: Hariharan Basuthkar <hbasuthk@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/reg.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath11k/reg.h
@@ -36,7 +36,7 @@ void ath11k_regd_update_work(struct work_struct *work);
 void ath11k_regd_update_chan_list_work(struct work_struct *work);
 struct ieee80211_regdomain *
 ath11k_reg_build_regd(struct ath11k_base *ab,
-		      struct cur_regulatory_info *reg_info, bool intersect);
+		      struct cur_regulatory_info *reg_info);
 int ath11k_regd_update(struct ath11k *ar);
 int ath11k_reg_update_chan_list(struct ath11k *ar, bool wait);
 enum wmi_reg_6ghz_ap_type
