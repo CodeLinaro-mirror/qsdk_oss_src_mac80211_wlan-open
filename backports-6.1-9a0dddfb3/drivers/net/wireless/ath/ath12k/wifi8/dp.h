b58bd7b510c79f1fb6ab77aa4015c37bc0f2cfe8
Dhananjay Latkar <dlatkar@qti.qualcomm.com>
2026-04-22 13:34:51 +0530

    QSDK:DP:wifi:ath12k: add extns stub for dp_wifi8 struct
    Add a new field to the dp_wifi8 struct to serve as a stub for extensions.
    This field will be used by the SDX-IPA extension and any future extensions.
    
    Patch-dependency: none
    
    Change-Id: I5a676795eb0dada06d09dd6fd49b317ad872a87d
    Signed-off-by: Dhananjay Latkar <dlatkar@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/dp.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/dp.h
@@ -12,6 +12,9 @@
 #include "hw.h"
 #include "dp_ast.h"
 #include "hal.h"
+#ifdef CPTCFG_QCN_EXTN
+#include "qcn_extns/dp.h"
+#endif
 
 #define DP_TX_EXCEPTION_RING_SIZE      512
 #define DP_WBM_REFILL_RING_MAX         4
@@ -104,6 +107,9 @@ struct ath12k_dp_wifi8 {
 	struct dp_srng ppe2wbm_idle_buf_ring;
 	struct dp_srng rx_peer_telemetry_ring;
 	struct dp_srng tx_peer_telemetry_ring;
+#ifdef CPTCFG_QCN_EXTN
+	struct ath12k_dp_wifi8_extn extn;
+#endif /* CPTCFG_QCN_EXTN */
 };
 
 struct ath12k_dp_hw_group_wifi8 {
