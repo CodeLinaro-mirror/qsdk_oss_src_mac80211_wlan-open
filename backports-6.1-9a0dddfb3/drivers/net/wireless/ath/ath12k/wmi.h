078f6a8dc1856df9656d8437e0569df51702c3cc
Maharaja Kennadyrajan <mkenna@qti.qualcomm.com>
2026-05-20 01:59:46 +0530

    QSDK:CP:wifi:ath12k: move DCS WLAN interference stats struct to common extn
    Move struct wmi_dcs_wlan_interference_stats definition out of ath12k WMI headers
    to common extension headers. Remove the duplicate definition from wmi.h as the
    structure is now provided by wlan-open-extns.
    This avoids duplication and ensures a single shared definition across ath12k
    extension code.
    
    Patch-work: None
    Patch-dependency: None
    
    Fixes: Iff87e3cfb47 ("QSDK: wifi: Add support for WLAN interference stats collect")
    
    Change-Id: I576cb7ba3ec21ca3282abe816f250d72af4ac6b3
    Signed-off-by: Maharaja Kennadyrajan <mkenna@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wmi.h
@@ -6073,24 +6073,6 @@ struct wmi_dcs_cw_info {
 	__le32 channel;
 };
 
-struct wmi_dcs_wlan_interference_stats {
-	u32 reg_tsf32;
-	u32 last_ack_rssi;
-	u32 tx_waste_time;
-	u32 rx_time;
-	u32 phyerr_cnt;
-	u32 listen_time;
-	u32 reg_tx_frame_cnt;
-	u32 reg_rx_frame_cnt;
-	u32 reg_rxclr_cnt;
-	u32 reg_cycle_cnt;
-	u32 reg_rxclr_ext_cnt;
-	u32 reg_ofdm_phyerr_cnt;
-	u32 reg_cck_phyerr_cnt;
-	s32 chan_nf;
-	u32 my_bss_rx_cycle_count;
-};
-
 struct wmi_dcs_wlan_interference_stats_ev {
 	__le32 reg_tsf32;
 	__le32 last_ack_rssi;
