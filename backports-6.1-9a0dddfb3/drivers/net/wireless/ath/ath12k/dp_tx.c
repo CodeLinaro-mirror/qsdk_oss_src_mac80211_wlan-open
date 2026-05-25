a82baee71592486c5f00246aec8b747b47bfe680
Selvavignesh S <selvavig@qti.qualcomm.com>
2026-05-25 11:10:29 +0530

    UPSTREAM:DP:wifi:ath12k: Enable SW delay calculation for VoW stats
    Patch-dependency: I118f0e0f38dc3279534fae45cb7bbda4c2ec1c20
    (UPSTREAM:DP:wifi:ath12k: Add SW delay calculation framework)
    
    Change-Id: I458cfa2543a4a217bfe4371996db94dc2b6af05f
    Signed-off-by: Selvavignesh S <selvavig@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_tx.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_tx.c
@@ -837,6 +837,7 @@ void ath12k_dp_tx_stats_update_pre_enqueue(struct ath12k_pdev_dp *dp_pdev,
 	}
 
 	if (ath12k_dp_delay_stats_enabled(dp_pdev) ||
+	    ath12k_dp_vow_stats_enabled(dp_pdev) ||
 	    unlikely(skb->mark & SDWF_VALID_MASK))
 		__net_timestamp(skb);
 
@@ -961,7 +962,8 @@ void ath12k_dp_tx_stats_post_enqueue(struct ath12k_dp *dp,
 				    ATH_TX_UNICAST_PKTS);
 	}
 
-	if (ath12k_dp_delay_stats_enabled(dp_pdev))
+	if (ath12k_dp_delay_stats_enabled(dp_pdev) ||
+	    ath12k_dp_vow_stats_enabled(dp_pdev))
 		tx_desc->hw_enqueue_tstamp = (u32)ktime_to_us(ktime_get_real());
 }
 EXPORT_SYMBOL(ath12k_dp_tx_stats_post_enqueue);
