e86dad75d517f8a94b8b69fa2fd03eaae170c4c4
Mohan Prabu <mprabub@qti.qualcomm.com>
2026-05-26 17:18:08 +0530

    QSDK:CP:wifi:ath12k: Add strict passive PCH scan radio parameter
    Add QCA_WLAN_VENDOR_RADIO_PARAM_SCAN_STRICT_PASSIVE_PCH to expose
    strict passive preferred channel scan configuration through the QCA
    vendor radio parameter interface.
    
    Add radio_cfg_extn to ath12k_radio_cfg under QCN extension support so
    extension-specific radio configuration is stored with the base radio
    configuration.
    
    TAG: PASSIVE SCAN
    
    Patch-dependency: None
    
    Patch-work: None
    
    Change-Id: I0be5a026d2d003f154b37aa9b8f5480b42768563
    Signed-off-by: Mohan Prabu <mprabub@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.h
@@ -1559,6 +1559,9 @@ struct ath12k_radio_cfg {
 	u32 regdomain;
 	bool chan144_enabled;
 	u32 pdev_to_reo_dest;
+#ifdef CPTCFG_QCN_EXTN
+	struct ath12k_radio_cfg_extn radio_cfg_extn;
+#endif
 };
 
 /**
