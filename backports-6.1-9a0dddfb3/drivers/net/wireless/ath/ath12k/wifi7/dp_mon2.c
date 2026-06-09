0adb60e7f2f62d35b1e847ac37f1ff36fb2b9b9d
Vishnu Vardhan Bantanahal <vishvard@qti.qualcomm.com>
2026-06-09 16:09:30 +0530

    UPSTREAM:DP:wifi:ath12k: Send notification to publish PPDU events in data path for wifi8.
    Hook the PPDU notifier framework into the data path to publish events on TX and RX PPDU completion.
    
    Current code generates events for TX when PPDU stats are pulled and for RX after peer and PPDU info resolution. This has a problem when no listeners are registered, as it leads to unnecessary SKB allocation and event preparation.
    
    We improve this by utilizing the framework's listener tracking to implement an early exit check, which allows the driver to bypass these allocations when there are no listeners.
    
    Additionally, changes ensure that MPDU Queues are re-initialized to a safe state for event_data, preventing any corrupted access from the Listener and ensuring that only scalar data is processed efficiently.
    
    Patch-dependency: I81848ca2001e464cb2967ddda242bff60e6ac891 ("QSDK:DP:wifi:ath12k: Add sample PPDU event listener")
    
    Change-Id: Ia56fbe655b79d9cd3b9cce3bc210e66ce3046a41
    Signed-off-by: Vishnu Vardhan Bantanahal <vishvard@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp_mon2.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi7/dp_mon2.c
@@ -1691,19 +1691,23 @@ static int ath12k_wifi7_dp_mon_rx_add_ppdu_desc(struct list_head *mon_desc_used_
 	return 0;
 }
 
-static void ath12k_dp_rx_mon_ppdu_notify(struct hal_rx_mon_ppdu_info *ppdu_info)
+static void ath12k_dp_rx_mon_ppdu_notify(struct ath12k_dp *dp,
+					 struct hal_rx_mon_ppdu_info *ppdu_info)
 {
 	struct ath12k_ppdu_event event;
 	struct sk_buff *skb;
 	struct ath12k_ppdu_rx_info *ppdu_evt_data;
 	unsigned int len;
 
-	if (ppdu_info->peer_id == HAL_INVALID_PEERID)
-		return;
-
 	/* Early exit if no one is listening for RX events - avoid unnecessary work */
 	if (!ath12k_ppdu_notifier_has_listeners(ATH12K_EVENT_PPDU_RX_COMPLETE))
 		return;
+
+	if (ppdu_info->peer_id == HAL_INVALID_PEERID)
+		return;
+
+	/* Send PPDU notification to registered listeners */
+	ppdu_info->device_id = ath12k_get_ab_device_id(dp->ab);
 
 	len = sizeof(*ppdu_evt_data);
 	skb = alloc_skb(len, GFP_ATOMIC);
@@ -1715,10 +1719,19 @@ static void ath12k_dp_rx_mon_ppdu_notify(struct hal_rx_mon_ppdu_info *ppdu_info)
 
 	ppdu_evt_data = skb_put_zero(skb, len);
 	memcpy(&ppdu_evt_data->ppdu_info, ppdu_info, sizeof(*ppdu_info));
+
+	/*
+	 * Re-initialize the MPDU queues to safe state
+	 * to avoid corrupted access by listener.
+	 */
+	for (int i = 0; i < HAL_MAX_UL_MU_USERS; i++)
+		skb_queue_head_init(&ppdu_evt_data->ppdu_info.mpdu_q[i]);
+
 	memset(&event, 0, sizeof(event));
 	event.skb = skb;
 
 	ath12k_ppdu_notifier_call_chain(ATH12K_EVENT_PPDU_RX_COMPLETE, &event);
+
 	/*
 	 * NOTE:
 	 * This skb is producer-owned.
@@ -1726,9 +1739,8 @@ static void ath12k_dp_rx_mon_ppdu_notify(struct hal_rx_mon_ppdu_info *ppdu_info)
 	 * Only skb_copy() is permitted if a separate buffer is needed.
 	 */
 	if (refcount_read(&skb->users) > 1)
-		ath12k_dbg(NULL, ATH12K_DBG_TELEMETRY,
-			   "SKB ref cnt held by Rx PPDU listener = %d\n",
-			   refcount_read(&skb->users));
+		ath12k_warn(dp, "SKB ref cnt held by Rx PPDU listener = %d\n",
+			    refcount_read(&skb->users));
 	kfree_skb(skb);
 }
 
@@ -1944,9 +1956,7 @@ ath12k_wifi7_dp_mon_rx_process_ppdu(struct work_struct *work)
 unlock:
 			spin_unlock_bh(&dp->dp_lock);
 			rcu_read_unlock_bh();
-			/* Send PPDU notification to registered listeners */
-			ppdu_info->device_id = ath12k_get_ab_device_id(dp->ab);
-			ath12k_dp_rx_mon_ppdu_notify(ppdu_info);
+			ath12k_dp_rx_mon_ppdu_notify(dp, ppdu_info);
 free_buf:
 			page_frag_free(status_desc->mon_buf);
 			mon_stats->status_buf_free++;
