01dade5e24f58127f65f54dc44a9a4820599d063
Abhinandan Kumar R <abhinan@qti.qualcomm.com>
2026-06-09 20:49:55 +0530

    UPSTREAM:DP:wifi:ath12k Move proto stats/free to common entry points
    Allocate and free protocol stats structure for peer level at common entry points
    
    Patch-dependency: None
    
    Change-Id: Iff7922861517ed1d32ae21baeb96155e029e8eb6
    Signed-off-by: Abhinandan Kumar R <abhinan@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_peer.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/dp_peer.c
@@ -1915,6 +1915,11 @@ int ath12k_dp_peer_stats_alloc(struct ath12k_dp_peer *dp_peer,
 		}
 	}
 
+	if (ath12k_proto_stats_enabled(dp_pdev)) {
+		ret = ath12k_dp_alloc_proto_stats_peer(dp_peer);
+		if (ret)
+			ath12k_warn(dp_pdev->ar->ab, "Failed to alloc proto stats.\n");
+	}
 	return ret;
 }
 EXPORT_SYMBOL(ath12k_dp_peer_stats_alloc);
@@ -1935,6 +1940,8 @@ void ath12k_dp_peer_stats_free(struct ath12k_dp_peer *dp_peer)
 
 	kfree(dp_peer->mld_stats.sojourn_stats);
 	dp_peer->mld_stats.sojourn_stats = NULL;
+
+	ath12k_dp_free_proto_stats_peer(dp_peer);
 }
 EXPORT_SYMBOL(ath12k_dp_peer_stats_free);
 
