5f3f39c74f57c60d472d8ec10d40222dc5cbca2b
Rajat Soni <rajson@qti.qualcomm.com>
2026-06-09 12:23:29 +0530

    QSDK:CP: wifi: ath12k: Fix bandwidth validation for CFR
    Currently, when the user configures a 3-link MLO setup with 2.4 GHz
    (20 MHz), 5 GHz (80 MHz), and 6 GHz (20 MHz), the station associates
    with all three links. However, when CFR capture is initiated from the AP
    on the 5 GHz interface with bandwidth 2 (80 MHz), an "invalid argument"
    error is returned.
    
    This occurs because the bandwidth validation is performed against
    sta->deflink.bandwidth, which is incorrect since the default link may
    correspond to the 6 GHz interface. As a result, the bandwidth of the
    5 GHz radio is incorrectly compared against the bandwidth of the 6 GHz
    radio, leading to unintended failures.
    
    Fix this issue by updating the validation check to compare against
    link_sta->bandwidth instead.
    
    Patch-work: None
    Patch-dependency: Idb0a655d250 ("ath12k: add CFR single shot/periodic capture support")
    
    Change-Id: I0d772cd0c1c20dd422052ac4d3634bd966b2ec3c
    Signed-off-by: Rajat Soni <rajson@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/debugfs_sta.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/debugfs_sta.c
@@ -1430,7 +1430,7 @@ static ssize_t ath12k_dbg_sta_write_cfr_capture(struct file *file,
 	}
 
 	if (cfr_capture_enable > WMI_PEER_CFR_CAPTURE_ENABLE ||
-	    cfr_capture_bw > sta->deflink.bandwidth ||
+	    cfr_capture_bw > link_sta->bandwidth ||
 	    cfr_capture_method > CFR_CAPURE_METHOD_NULL_FRAME_WITH_PHASE ||
 	    cfr_capture_period > WMI_PEER_CFR_PERIODICITY_MAX) {
 		ret = -EINVAL;
