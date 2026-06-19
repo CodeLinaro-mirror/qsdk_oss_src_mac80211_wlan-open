5b84cd8a221783600291b826e8d356e85f45f21d
Thirusenthil Kumaran J <thirusen@qti.qualcomm.com>
2026-06-19 12:18:13 +0530

    QSDK:PROTOCOL:wifi:ath12k: Wire ath12k_wifi8_hw_init_extn into wifi8 hw init
    Call ath12k_wifi8_hw_init_extn(ab) at the end of
    ath12k_wifi8_hw_init(), to handle extn initialisation for wifi8 HW.
    
    Add qcn_extns/wifi8_hw_extn.o to the CPTCFG_QCN_EXTN
    object list in the wifi8 Makefile so the new extension
    file is compiled when extensions are enabled.
    
    Feature: AFC / 6 GHz
    Tag: Regulatory / AFC
    Patch-work: None
    Patch-dependency: None
    
    Change-Id: Ib53b80ca801a01ddcf7dbb7033bd169a10b70afa
    Signed-off-by: Thirusenthil Kumaran J <thirusen@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/wifi8/hw.c
@@ -1465,5 +1465,9 @@ int ath12k_wifi8_hw_init(struct ath12k_base *ab)
 
 	ath12k_info(ab, "WiFi8 Hardware name: %s\n", ab->hw_params->name);
 
+#ifdef CPTCFG_QCN_EXTN
+	ath12k_wifi8_hw_init_extn(ab);
+#endif
+
 	return 0;
 }
