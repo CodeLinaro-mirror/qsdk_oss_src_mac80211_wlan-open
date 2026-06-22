a343d11ca2dc36a3e0aaf52551053a11f5302f86
Sai Swetha Manonmayee Bharatula <sbharatu@qti.qualcomm.com>
2026-06-22 00:09:02 -0700

    Revert "UPSTREAM:PLATFORM: wifi: ath12k: configure secure registers for IO coherency (IOC)"
    This reverts commit cd21955573e209fa0523bd64f1eb32454e9a8258.
    
    Reason for revert: <Fails for RDP 466, need to update the HW_rv>
    
    Patch-dependency: None
    
    Change-Id: I6ff416c326578a9b254a876fadea98cc49cf1543
    Signed-off-by: Sai Swetha Manonmayee Bharatula <sbharatu@qti.qualcomm.com>

--- backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.c
+++ backports-6.1-9a0dddfb3/drivers/net/wireless/ath/ath12k/core.c
@@ -461,17 +461,12 @@ extern struct ath12k_coredump_info ath12k_coredump_ram_info;
 #ifdef CONFIG_IO_COHERENCY
 int ath12k_core_config_iocoherency(struct ath12k_base *ab, bool enable)
 {
-	int ret = 0, num_elem, idx = 0;
+	int ret, num_elem, idx = 0;
 	struct tmel_secure_io secure_reg;
 
 	if (!ath12k_io_coherency_enabled) {
 		ath12k_err(ab, "io-coherency Disabled\n");
 		return 0;
-	}
-
-	if (!ab->dev || !ab->dev->of_node) {
-		ath12k_err(ab, "improper dts node\n");
-		return -ENODEV;
 	}
 
 	if (ab->in_coldboot_fwreset)
@@ -488,7 +483,7 @@ int ath12k_core_config_iocoherency(struct ath12k_base *ab, bool enable)
 		ret = of_property_read_u32_index(ab->dev->of_node, "secure-reg", idx++,
 						 &secure_reg.reg_addr);
 		if (ret) {
-			ath12k_err(ab, "failed to get addr %d\n", (idx - 1));
+			ath12k_err(ab, "failed to get the secure reg addr %d\n", (idx - 1));
 			goto err;
 		}
 
@@ -497,8 +492,7 @@ int ath12k_core_config_iocoherency(struct ath12k_base *ab, bool enable)
 							 &secure_reg.reg_val);
 
 			if (ret) {
-				ath12k_err(ab, "failed to get reg val %d\n",
-					   (idx - 1));
+				ath12k_err(ab, "failed to get the secure reg val %d\n", (idx - 1));
 				goto err;
 			}
 		} else {
@@ -508,28 +502,16 @@ int ath12k_core_config_iocoherency(struct ath12k_base *ab, bool enable)
 
 		ath12k_info(ab, "Configuring secure reg: 0x%x val: 0x%x\n",
 			    secure_reg.reg_addr, secure_reg.reg_val);
-		if (ab->hw_rev == ATH12K_HW_IPQ5424_HW10) {
-			ret = tmelcom_secure_io_write(&secure_reg,
-						      sizeof(struct tmel_secure_io));
-			if (ret) {
-				ath12k_err(ab, "Failed secure_reg settings\n");
-				goto err;
-			}
 
-		} else {
-			void __iomem *map_addr;
+		ret = tmelcom_secure_io_write(&secure_reg, sizeof(struct tmel_secure_io));
 
-			map_addr = ioremap(secure_reg.reg_addr,
-					   sizeof(uint32_t));
-			if (!map_addr) {
-				ret = -ENOMEM;
-				ath12k_err(ab, "Failed secure_reg mapping\n");
-				goto err;
-			}
-			writel(secure_reg.reg_val, map_addr);
-			iounmap(map_addr);
+		if (ret) {
+			ath12k_err(ab, "Failed to update secure_reg settings, ret = %d reg: 0x%x val: 0x%x\n",
+				   ret, secure_reg.reg_addr, secure_reg.reg_val);
+			goto err;
 		}
 	}
+
 err:
 	return ret;
 }
